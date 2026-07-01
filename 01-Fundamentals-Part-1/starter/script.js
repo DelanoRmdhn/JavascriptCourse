47e1f9eb042924c73e59957a501eff2c791192cc
Delano <del.191006@gmail.com>
2026-07-01 17:31:24 +0700

    1. Data Types : Primitive

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,84 +1,30 @@
-/* MINI TASK HARIAN */
+//DAY 2
 
-// 1. Values & Variables
+/* 
 
-/*SOAL 1
-Values and Variables
-Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+DATA TYPES : Terdapat 2 Jenis Data Types yakni Object & Primitive. untuk saat ini kita akan berfokus di tipe data jenis primitive.
 
-Log their values to the console.
+1. Number : Tipe data berjenis angka, contoh : 23
+2. String : Tipe data berjenis String, contoh : "Delano Bariq Ramadhan 19"
+3. Boolean : Tipe data dengan jenis kondisi, contoh : true / false
+4. Undefined : kondisi dimana sebuah variable belum memiliki nilai, contoh : let namaSaya;
+5. Null : kosong
+6. Symbol : tipe data yang dimana nilainya bersifat unik dan tidak dapat diubah
+7. BigInt : digunakan ketika tipe data number sudah tidak bisa menampung ukuran angka yang disimpan
+
+typeof : untuk melihat jenis tipe data yang digunakan
 */
 
-let country = "Indonesia";
-let continent = "Asia";
-let population = "240 Milion";
+let nama = "Delano Bariq Ramadhan";
+console.log(nama);
+console.log("Jenis tipe data variable nama : " + typeof nama);
 
-console.log("Country : " + country);
-console.log("Continent : " + continent);
-console.log("Population : " + population);
+let umur;
+console.log(umur);
+console.log(
+  "Case apabila Variabel umur belum di set value nya maka : " + typeof umur,
+);
 
-/*SOAL 2
-Berikut adalah format Soal Praktikum Mandiri untuk Day 1, lengkap dengan skenario bisnis, instruksi pengerjaan, dan kriteria pengujian untuk melatih fundamental pemrograman kamu.
-
-SOAL PRAKTIKUM MANDIRI - DAY 1
-Topik: Pengenalan JavaScript, Eksternal File, Variabel, dan Tipe Data Dasar.
-Studi Kasus: Pengaturan Awal Data Agensi Pembuatan Website UMKM.
-
-Skenario Soal
-Anda sedang membangun sebuah agensi digital yang berfokus pada jasa pembuatan website untuk UMKM dan Arsitek. Sebelum membuat tampilan UI yang kompleks di browser, Anda diminta oleh tim senior untuk menyiapkan fondasi arsitektur data awal agensi Anda di sisi JavaScript. Data ini nantinya akan digunakan untuk kalkulasi otomatis pada sistem kasir dan komponen React di masa mendatang.
-
-Instruksi Tugas
-Deklarasi Data Variabel (di dalam script.js):
-Buatlah variabel-variabel berikut dengan memilih kata kunci deklarasi yang tepat (const atau let) berdasarkan sifat nilainya:
-
-namaAgensi (Tipe Data: String): Berisi nama agensi web development pilihan Anda. (Nilai tidak boleh berubah).
-
-tahunBerdiri (Tipe Data: Number): Tahun sekarang agensi Anda resmi berjalan (2026).
-
-hargaLandingPage (Tipe Data: Number): Nilai harga dasar paket landing page (contoh: 4000000). Jangan gunakan titik atau koma sebagai pemisah ribuan.
-
-hargaCompanyProfile (Tipe Data: Number): Nilai harga dasar paket company profile (contoh: 8000000).
-
-isOpenForBooking (Tipe Data: Boolean): Menandakan status apakah agensi Anda saat ini sedang membuka kuota untuk klien baru atau tidak (true/false).
-
-fiturUnggulan (Tipe Data: String): Kalimat pendek yang menjelaskan kelebihan desain agensi Anda (misal: "Integrasi Figma ke Code dengan Detail dan Presisi").
-
-Output Pengujian:
-
-Tampilkan semua variabel tersebut ke dalam Console Browser menggunakan perintah console.log().
-
-Berikan label penjelas pada setiap output agar teks di console terbaca rapi oleh tim penguji.
-
-Kriteria Penilaian & Kelulusan (Checklist Mandiri)
-[ ] File HTML berhasil memuat file JS eksternal tanpa memicu error File not found.
-
-[ ] Variabel nama agensi menggunakan const (karena nama perusahaan bersifat permanen).
-
-[ ] Tipe data harga dideklarasikan sebagai Number murni (contoh: 4000000), BUKAN sebagai string bertanda kutip (seperti "4.000.000"), agar nantinya bisa dihitung secara matematis pada hari Selasa.
-
-[ ] Variabel boolean ditulis murni tanpa tanda kutip (true / false).
-
-[ ] Saat file HTML dibuka di browser dan ditekan F12 (Menu Console), data profil agensi tercetak dengan rapi tanpa ada Red Error Text (Syntax Error/Reference Error).
-
-*/
-
-const namaAgensi = "PT.CAHAYA REMBULAN SEJATI";
-const tahunBerdiri = 2026;
-let hargaLandingPage = 1500000;
-let hargaCompanyProfile = 3000000;
-let isOpenForBooking = true;
-let fiturUnggulan = "Integrasi Figma ke Code dengan Detail dan Presisi";
-
-console.log("Nama Agensi : " + namaAgensi);
-console.log("Tahun Berdiri : " + tahunBerdiri);
-console.log("Harga Landing Page : " + hargaLandingPage);
-console.log("Harga Company Profile : " + hargaCompanyProfile);
-console.log("Apakah kita sedang open for booking : " + isOpenForBooking);
-console.log("Fitur Unggulan : " + fiturUnggulan);
-
-/*REASON PEMILIHAN VARIABLE
-
-1. const = variable namaAgensi & tahunBerdiri kita pilih menggunakan const karena nilainya bersifat tetap dan tidak akan diganti kedepannya. --> nilai Statis
-
-2. let = variable selain namaAgensi & tahunBerdiri kita pilih menggunakan let karena kedepannya bisa jadi saya mau melakukan perubahan value pada variable variable tersebut. --> Nilai Dinamis
-*/
+umur = 2026 - 2006;
+console.log(umur);
+console.log("Variable Umur telah di set dan nilainya : " + umur);
