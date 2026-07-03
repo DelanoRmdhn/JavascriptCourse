b1500b73690f6d897880edbac8b19a85b56c33ca
Delano <del.191006@gmail.com>
2026-07-03 19:08:01 +0700

    2. Truthy & Falsy Values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,21 +1,28 @@
-// DAY 4
+//DAY 4
 
 /*
-TYPE CONVERSION & TYPE COERCION
+TRUTHY VS FALSY VALUES
 
-type Conversion : Mengubah tipe data sebuah nilai secara Manual / kita ubah sendiri
-type coercion : Mengubah tipe data sebuah nilai secara otomatis
+adalah nilai yang dikategorikan sebagai benar dan salah.
 
+contoh truthy values               contoh falsy values
+1. angka selain 0                  1. 0            
+2. {}                              2. NaN
+3. String yang ada isinya          3. Undefined
 */
 
-let input = prompt("Masukan tahun kelahiran anda :");
-console.log(typeof input); // dia bertipe data String.
+//contoh falsy
+let money = 0;
+if (money) {
+  console.log(`Foya Foya dlu ga siee :)`);
+} else {
+  console.log(`Nabung bro duit kamu Rp.${money} di bank :(`);
+}
 
-//cara conversion
-let tahunKelahiran = Number(input);
-console.log(typeof tahunKelahiran); //sekarang dia tipe data nya sudah Number
-
-console.log(`Maka Umur Kamu sekarang adalah ${2026 - tahunKelahiran} tahun.`);
-
-//kalo case coercion
-console.log(`Maka Umur Kamu sekarang adalah ${2026 - input} tahun.`); // ini bisa terjadi karena javascript secara otomatis mengubah tipe data nya si variable input karena ada operasi aritmatika (-,*,/) di dalamnya. namun apabila menggunakan + maka dia tetap ke detect sebagai String.
+//contoh Truthy
+money = 100;
+if (money) {
+  console.log(`Foya Foya dlu ga siee :)`);
+} else {
+  console.log(`Nabung bro duit kamu Rp.${money} di bank :(`);
+}
