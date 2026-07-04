a6054ddc338d666d5708f88743097a75c95682c0
Delano <del.191006@gmail.com>
2026-07-04 15:20:37 +0700

    3. Expression VS Statement & Conditional Operator (Ternary)

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -2,36 +2,24 @@
 
 /*
 
-SWITCH STATEMENT : digunakan ketika ingin membandingkan satu nilai dari banyak variabel lain. 
+STATEMENT & EXPRESSION
+
+Statement = menjalankan sebuah aksi atau instruksi tapi dia di treat BUKAN sebagai nilai. contoh : if-else, switch, let a = 12.
+Expression = Expression menghasilkan sebuah nilai. contoh = 2+4,1994,"Delano"
+
+
+CONDITIONAL OPERATOR / TERNARY OPERATOR
+Mirip seperti if else namun yang membedakan adalah Conditional Operator ialah  operator kondisional yang bentuknya expression.
 */
 
-const input = prompt("Masukan Hari Favoritmu : ");
+let umur = 19;
+const usiaLegal = umur >= 18 ? "Kamu sudah dewasa" : "Kamu masih bocil";
+console.log(usiaLegal);
 
-switch (input) {
-  case "senin":
-    console.log(`Senin adalah Hari Favoritmu!`);
-    break;
+console.log(
+  `Hai Nama Saya Delano, umur saya ${umur} dan ini menandakan bahwa saya ${umur >= 18 ? "Sudah Dewasa" : "Masih Bocil"}`,
+);
 
-  case "selasa":
-    console.log(`Selasa adalah Hari Favoritmu!`);
-    break;
-
-  case "rabu":
-    console.log(`Rabu adalah Hari Favoritmu!`);
-    break;
-  case "Kamis":
-    console.log(`Kamis adalah Hari Favoritmu!`);
-    break;
-  case "Jumaat":
-    console.log(`Jumaat adalah Hari Favoritmu!`);
-    break;
-  case "Sabtu":
-    console.log(`Sabtu adalah Hari Favoritmu!`);
-    break;
-  case "Minggu":
-    console.log(`Minggu adalah Hari Favoritmu!`);
-    break;
-
-  default:
-    console.log(`Tidak ada hari dengan nama ${input}`);
-}
+let nilai = 85;
+const grade = nilai >= 75 ? "Lulus" : "tidak lulus";
+console.log(grade);
