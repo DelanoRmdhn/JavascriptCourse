b507661cc12b748a7de318548ba8d78ce43c003f
Delano <del.191006@gmail.com>
2026-07-04 14:41:51 +0700

    2. Switch Statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,30 +1,37 @@
 //DAY 5
 
 /*
-EQUALITY OPERATORS : == VS ===
 
-Adalah sebuah operator untuk memvalidasi / membandingkan suatu nilai yang mengembalikan sebuah nilai berupa true atau false.
-
-== : loose equality operator. Type coercion diterapkan. 
-
-=== : ini adalah strict equality operator. yang berarti type coercion tidak diterapkan. Dimana dia hanya akan mengembalikan nilai true apabila kedua nilai sama persis. (gunakan ini untuk menghindari Bug)  
+SWITCH STATEMENT : digunakan ketika ingin membandingkan satu nilai dari banyak variabel lain. 
 */
 
-const umur1 = 20;
-const umur2 = 20;
-//===
-if (umur1 === 20) console.log(`Kamu sudah dewasa! umur anda ${umur1} tahun.`);
-//==
-if (umur2 == "20") console.log(`Kamus sudah dewasa! umur anda ${umur2} tahun.`);
+const input = prompt("Masukan Hari Favoritmu : ");
 
-let input = prompt(`Masukan Angka : `);
+switch (input) {
+  case "senin":
+    console.log(`Senin adalah Hari Favoritmu!`);
+    break;
 
-//latihan penasaran
-if (!Number.isNaN(Number(input))) {
-  input = Number(input);
-  console.log(
-    `Angka Favorit Kamu adalah : ${input} tipe datanya : ${typeof input}`,
-  );
-} else {
-  console.log(`Yang kamu Input Bukan angka!`);
+  case "selasa":
+    console.log(`Selasa adalah Hari Favoritmu!`);
+    break;
+
+  case "rabu":
+    console.log(`Rabu adalah Hari Favoritmu!`);
+    break;
+  case "Kamis":
+    console.log(`Kamis adalah Hari Favoritmu!`);
+    break;
+  case "Jumaat":
+    console.log(`Jumaat adalah Hari Favoritmu!`);
+    break;
+  case "Sabtu":
+    console.log(`Sabtu adalah Hari Favoritmu!`);
+    break;
+  case "Minggu":
+    console.log(`Minggu adalah Hari Favoritmu!`);
+    break;
+
+  default:
+    console.log(`Tidak ada hari dengan nama ${input}`);
 }
