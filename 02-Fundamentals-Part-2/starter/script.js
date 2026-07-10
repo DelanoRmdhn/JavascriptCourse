a2671eac0e585faef1720f502c0faa3f4aefffc8
Delano <del.191006@gmail.com>
2026-07-10 22:09:00 +0700

    3. Nested Loop (bermain main dengan Nested Loop)

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -11,19 +11,75 @@ kondisi : melakukan pengecekan kondisi, apabila true maka loop berjalan, dan apa
 update : bagian ini memberikan perubahan / update pada nilai penghitung setiap 1 putaran loop selesai
 
 For Loop pada array bisa kita gunakan untuk mengakses atau memproses setiap elemen pada array secara berurutan menggunakan indeksnya.
+
+NESTED LOOP : Pada nested loop, setiap kali loop luar melakukan satu iterasi, loop dalam akan dijalankan sampai selesai terlebih dahulu. Setelah loop dalam selesai, barulah loop luar melanjutkan ke iterasi berikutnya.
 */
 
-const tahunKelahiran = [2006, 1976, 2009, 2017, 1983];
-const usia = [];
-for (let i = 0; i < tahunKelahiran.length; i++) {
-  if (typeof tahunKelahiran[i] !== "number") continue; //continue : melanjutkan ke perulangan selanjutnya apabila kondisi benar
-  usia.push(2026 - tahunKelahiran[i]);
+for (let i = 1; i <= 3; i++) {
+  console.log(`Perulangan Luar ke-${i}`);
+  for (let j = 1; j <= 2; j++) {
+    console.log(i, j);
+  }
 }
 
-console.log(usia);
+//SORTING ANGKA DARI KECIL KE BESAR
+const angka = [7, 9, 3, 5, 1];
+let temp;
 
-for (let i = 0; i < tahunKelahiran.length; i++) {
-  usia[i] = 2026 - tahunKelahiran[i];
-  console.log(usia[i]);
-  if (typeof tahunKelahiran[i] === "number") break; //break : menghentikan seluruh perulangan apabila kondisi benar
+for (let i = 0; i < angka.length; i++) {
+  for (let j = i + 1; j < angka.length; j++) {
+    if (angka[i] > angka[j]) {
+      // console.log(angka[i], angka[j]);
+      temp = angka[j];
+      angka[j] = angka[i];
+      angka[i] = temp;
+    }
+  }
+}
+
+console.log(angka);
+
+/*
+Gunakan nested loop untuk menghasilkan:
+
+*
+**
+***
+****
+*****
+*/
+
+for (let i = 1; i <= 5; i++) {
+  let bintang = "";
+  for (let j = 0; j < i; j++) {
+    bintang += "*";
+  }
+  console.log(bintang);
+}
+
+for (let i = 1; i <= 5; i++) {
+  let angka = "";
+  for (let j = 1; j <= 5; j++) {
+    angka += i;
+  }
+
+  console.log(angka);
+}
+
+for (let i = 1; i <= 5; i++) {
+  let angka = "";
+  for (let j = 1; j <= 5; j++) {
+    angka += j;
+  }
+  console.log(angka);
+}
+
+for (let i = 1; i <= 5; i++) {
+  let angka = "";
+
+  for (let j = 1; j <= i; j++) {
+    angka += j;
+  }
+
+  console.log(angka);
 }
