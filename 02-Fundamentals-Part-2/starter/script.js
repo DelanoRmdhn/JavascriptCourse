0fd3e4bd2489f7b9ea167aa9a047ab12d9c9aec7
Delano <del.191006@gmail.com>
2026-07-10 20:53:52 +0700

    2. Looping pada Array (For Loop, Continue & Break)

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -9,24 +9,21 @@ Bentuk For Loop = for (inisialisasi ; kondisi ; update) {}
 insialisasi : buat variabel penghitung
 kondisi : melakukan pengecekan kondisi, apabila true maka loop berjalan, dan apabila tidak, loop berhenti
 update : bagian ini memberikan perubahan / update pada nilai penghitung setiap 1 putaran loop selesai
+
+For Loop pada array bisa kita gunakan untuk mengakses atau memproses setiap elemen pada array secara berurutan menggunakan indeksnya.
 */
 
-console.log(6 % 3);
-
-for (let i = 1; i <= 15; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else {
-    console.log(i);
-  }
+const tahunKelahiran = [2006, 1976, 2009, 2017, 1983];
+const usia = [];
+for (let i = 0; i < tahunKelahiran.length; i++) {
+  if (typeof tahunKelahiran[i] !== "number") continue; //continue : melanjutkan ke perulangan selanjutnya apabila kondisi benar
+  usia.push(2026 - tahunKelahiran[i]);
 }
 
-let hasil = "";
-for (let i = 1; i <= 5; i++) {
-  hasil += "#";
-  console.log(hasil);
+console.log(usia);
+
+for (let i = 0; i < tahunKelahiran.length; i++) {
+  usia[i] = 2026 - tahunKelahiran[i];
+  console.log(usia[i]);
+  if (typeof tahunKelahiran[i] === "number") break; //break : menghentikan seluruh perulangan apabila kondisi benar
 }
