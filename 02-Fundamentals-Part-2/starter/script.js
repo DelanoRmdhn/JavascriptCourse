c2db27f0a9538c0dffa40b39b317d58cc567d377
Delano <del.191006@gmail.com>
2026-07-11 20:10:11 +0700

    8. Mini Task Day 9 : Latihan 10 Soal For Loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,74 +1,170 @@
 "use strict";
 
-//MINI TASK MINGGU 2 DAY 8 : OBJECT
+//MINI TASK MINGGU 2 DAY 9 : 10 SOAL FOR LOOP
 
-/*SOAL 2
-Topik: Object Properties, Object Methods, & Kata Kunci this
+/*
+Soal 1 - Hitung 1 sampai 10
 
-Studi Kasus: Sistem Manajemen Piutang Klien UMKM Arsitektur
+SOAL 1
+Gunakan for untuk mencetak angka:
 
-Skenario
-Agensi PT. CAHAYA REMBULAN SEJATI baru saja mendapatkan proyek pembuatan Company Profile dari sebuah UMKM Firma Arsitektur lokal. Untuk menjaga arus kas (cash flow) agensi tetap sehat, Anda diminta membuat sebuah sistem objek digital yang mampu mencatat data kontrak pengerjaan serta menghitung secara otomatis apakah klien tersebut sudah melunasi pembayarannya sesuai kesepakatan atau belum.
-
-Instruksi Tugas
-Inisialisasi Data Objek:
-
-Buatlah sebuah objek bernama klienUMKM.
-
-Di dalam objek tersebut, buat properti-properti berikut beserta nilainya:
-
-namaPerusahaan: "Studio Arsitek Garis Lurus"
-
-jenisPaket: "Company Profile"
-
-totalKontrak: 5000000 (Angka murni nominal Rp5.000.000)
-
-jumlahYangDibayar: 3500000 (Angka murni nominal Rp3.500.000)
-
-
-
-Buat Fungsi Internal (Object Method):
-
-Di dalam objek klienUMKM yang sama, buat sebuah method bernama cekStatusPelunasan.
-
-Method ini tidak boleh menerima parameter dari luar. Dia harus memanfaatkan kata kunci this untuk mengakses data keuangan miliknya sendiri di dalam objek.
-
-Logika Pengamanan Keuangan:
-
-Hitung sisa tagihan secara mandiri di dalam fungsi tersebut.
-
-Jika jumlahYangDibayar bernilai sama dengan atau lebih besar dari totalKontrak, method harus mengembalikan (return) teks: "Lunas! Proyek siap dideploy ke server Next.js."
-
-Jika jumlahYangDibayar masih kurang dari totalKontrak, method harus mengembalikan teks yang dinamis: "Belum Lunas! Kurang Rp.[nominal_sisa_tagihan]. Tolong hubungi pihak klien." (Hitung sisa uangnya secara dinamis).
-
-Output Console:
-
-Cetak header pelaporan keuangan yang rapi ke console.
-
-Panggil method klienUMKM.cekStatusPelunasan() di dalam console.log() untuk melihat hasil evaluasi sistem secara nyata.
-
-Target Tampilan Output Console:
-Plaintext
-=== FINANSIAL LOG KLIEN UMKM ===
-Perusahaan      : Studio Arsitek Garis Lurus
-Paket Layanan   : Company Profile
-Status Finansial: Belum Lunas! Kurang Rp.1500000. Tolong hubungi pihak klien.
+1
+2
+3
+4
+...
+10
 */
 
-const klienUMKM = {
-  namaPerusahaan: "Studio Arsitek Garis Lurus",
-  jenisPaket: "Company Profile",
-  totalKontrak: 5000000,
-  jumlahYangDibayar: 3500000,
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
 
-  cekStatusPelunasan: function () {
-    return this.jumlahYangDibayar >= this.totalKontrak
-      ? `Lunas! Proyek siap dideploy ke server Next.js`
-      : `Belum Lunas! Kurang Rp. ${this.totalKontrak - this.jumlahYangDibayar}. Tolong hubungi pihak klien!`;
-  },
-};
+/*SOAL 2
+Soal 2 - Hitung Mundur
 
-console.log(`=== FINANSIAL LOG KLIEN UMKM ===\n
-Perusahaan      : ${klienUMKM.namaPerusahaan}\n
-Paket Layanan   : ${klienUMKM.jenisPaket}\n
-Status Finansial: ${klienUMKM.cekStatusPelunasan()}`);
+Output:
+
+10
+9
+8
+...
+1
+*/
+
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
+
+/*SOAL 3
+Soal 3 - Bilangan Genap
+
+Cetak semua bilangan genap dari 2 sampai 20.
+
+Output
+
+2
+4
+6
+...
+20
+*/
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+/*Soal 4 - Bilangan Ganjil
+
+Cetak semua bilangan ganjil dari 1 sampai 19. */
+
+for (let i = 1; i <= 19; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+/*
+Soal 5 - Jumlah Bilangan
+
+Hitung jumlah angka dari 1 sampai 100.
+Output
+5050
+*/
+
+let angka = 0;
+for (let i = 1; i <= 100; i++) {
+  angka += i;
+}
+
+console.log(angka);
+
+/*
+Soal 6 - Faktorial
+
+Hitung faktorial dari angka 5.
+5! = 120
+*/
+
+let hasil = 1;
+for (let i = 5; i >= 1; i--) {
+  hasil = hasil * i;
+}
+console.log(hasil);
+
+/*SOAL 7 Cetak semua kelipatan 3 dari 1 sampai 50. */
+for (let i = 1; i <= 50; i++) {
+  if (i % 3 === 0) {
+    console.log(i);
+  }
+}
+
+/*Soal 8 - Pangkat
+Hitung
+2^8
+Tanpa menggunakan ** ataupun Math.pow(). */
+
+let hasilPangkat = 1;
+for (let i = 1; i <= 8; i++) {
+  hasilPangkat *= 2;
+}
+console.log(hasilPangkat);
+
+/*Soal 9 - Balik Hitungan
+
+Cetak
+
+100
+90
+80
+70
+...
+0 */
+
+for (let i = 100; i >= 0; i -= 10) {
+  console.log(i);
+}
+
+/*Soal 10 - FizzBuzz (Latihan Legendaris)
+
+Cetak angka 1 sampai 30.
+
+Aturan:
+
+kelipatan 3 → "Fizz"
+kelipatan 5 → "Buzz"
+kelipatan 3 dan 5 → "FizzBuzz"
+selain itu cetak angkanya.
+
+Contoh
+
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+... */
+
+for (let i = 1; i <= 30; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
