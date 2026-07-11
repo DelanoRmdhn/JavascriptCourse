ad2c8905178ef41b305a61eaa26a8c8a344c1f23
Delano <del.191006@gmail.com>
2026-07-11 23:17:27 +0700

    11. Mini Task Day 9 : Studi Kasus Soal 1 (Tips Calculator)

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,128 +1,59 @@
 "use strict";
 
-//MINI TASK MINGGU 2 DAY 9 : 10 SOAL WHILE LOOP
+//MINI TASK STUDI KASUS SOAL 1
 
-// Soal 1
-// Cetak angka 1–10 menggunakan while.
-let counter = 1;
+/*Let's improve Steven's tip calculator even more, this time using loops!
 
-while (counter <= 10) {
-  console.log(counter);
-  counter++;
+Your tasks:
+
+Create an array called bills containing all 10 test bill values.
+
+Create empty arrays for the tips and the totals (tips and totals)
+
+Use the calcTip function we wrote before (included in the starter code) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+
+
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86, and 52.
+BONUS:
+
+Write a function calcAverage which takes an array called arr as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it if you feel like it:
+
+First, you will need to add up all values in the array. To do the addition, start by creating a variable sum that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
+
+To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements).
+
+Call the function with the totals array.
+
+
+
+👋 OPTIONAL: You can watch my solution in video format in the next lecture */
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+for (let i = 0; i < bills.length; i++) {
+  totals.push(bills[i] + calcTip(bills[i]));
+  tips.push(calcTip(bills[i]));
 }
 
-// Soal 2
-// Cetak angka 10–1 menggunakan while.
+console.log(tips);
+console.log(totals);
 
-counter = 10;
-while (counter >= 1) {
-  console.log(counter);
-  counter--;
-}
+/*BONUS */
 
-// Soal 3
-// Cetak semua bilangan genap dari 2–20.
-
-counter = 2;
-while (counter <= 20) {
-  if (counter % 2 === 0) {
-    console.log(counter);
+const calcAverage = function (arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
   }
-  counter++;
-}
+  let average = total / arr.length;
+  return average;
+};
 
-//Soal 4
-// Hitung jumlah angka 1–50.
-let hasil = 0;
-counter = 1;
-
-while (counter <= 50) {
-  hasil += counter;
-  counter++;
-}
-console.log(hasil);
-
-// Soal 5
-// Hitung faktorial 6.
-
-hasil = 1;
-counter = 6;
-while (counter >= 1) {
-  hasil = hasil * counter;
-  counter--;
-}
-
-console.log(hasil);
-
-//Soal 6
-// Cetak
-// 5
-// 10
-// 15
-// 20
-// ...
-// 100
-
-counter = 5;
-while (counter <= 100) {
-  console.log(counter);
-  counter += 5;
-}
-
-/* 
-Buat countdown
-
-10
-9
-8
-...
-Lift Off!
-*/
-
-counter = 10;
-while (counter > 0) {
-  console.log(counter);
-  counter--;
-  if (counter === 0) {
-    console.log(`Lift Off!`);
-  }
-}
-
-/* 
-Hitung banyak digit sebuah angka.
-
-Misalnya
-
-const angka = 123456;
-Output
-6
-
-Hint:
-Gunakan pembagian 10.
-*/
-
-let angka = 123456;
-counter = 0;
-while (angka !== 0) {
-  angka = Math.floor(angka / 10);
-  counter++;
-}
-console.log(counter);
-
-/*
-Soal 9
-
-Balik angka.
-
-Misalnya
-
-12345
-
-menjadi
-
-54321
-
-Hint:
-
-Gunakan % dan Math.floor().
-*/
+console.log(calcAverage(totals));
