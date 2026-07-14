4c754dade91eb11415dd25b8878969355ad1df7f
DelanoRmdhn <del.191006@gmail.com>
2026-07-14 18:16:25 +0700

    Implementing Game Logic 1 (Generate Random Number,Input Validation, Game Logic Phase 1)

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,29 +1,54 @@
 'use strict';
 
 /* 
-WEEK 3 : DOM & EVENTS IN JAVASCRIPT
+WEEK 3 : IMPLEMENTING GAME LOGIC
 
-//EVENTLISTENER 
+berikut ini adalah beberapa game logic yang diperlukan : 
+1. buat sebuah angka acak dengan rentang dari 1 - 10
+2. menunjukan apakah input user benar, lebih kecil, atau lebih besar dari angka tebakan yang di generate
+
 */
 
-const btnCheck = document.querySelector('.btn.check');
+//generate angka random dari 1 - 20
+const randomNumber = Math.trunc(Math.random() * 20) + 1;
+
+//simpan score
+let currentScore = 10;
+
+//TAG HTML yang mau dimanipulasi
 const inputNumber = document.querySelector('.guess');
-const paragraf = document.querySelector('.message');
+const btnCheck = document.querySelector('.btn.check');
+const message = document.querySelector('.message');
+const score = document.querySelector('.score');
 
-let guess = [];
+console.log(randomNumber);
+//FUNCTION UNTUK BTN CHECK
 btnCheck.addEventListener('click', function () {
-  guess.push(inputNumber.value);
-  console.log(typeof inputNumber.value);
-  console.log(guess);
-});
+  //ambil hasil inputan user
+  const inputtedNumber = Number(inputNumber.value);
 
-btnCheck.addEventListener('click', function () {
-  if (Number(inputNumber.value) === 10) {
-    paragraf.textContent = '🎉Benar!!!';
-  } else {
-    paragraf.textContent =
-      Number(inputNumber.value) > 10
-        ? '⬆ ANGKANYA KEBESARAN!'
-        : '⬇ ANGKANYA KEKECILAN!';
+  console.log(typeof inputtedNumber, inputtedNumber);
+
+  //logic game
+  if (!inputtedNumber) {
+    message.textContent = `❌ Input Your Guess!`;
+  } else if (inputtedNumber < 1 || inputtedNumber > 20) {
+    message.textContent = `❌ Number must be between 1 - 20. your guess : ${inputtedNumber}`;
+  } else if (inputtedNumber !== randomNumber && currentScore > 0) {
+    message.textContent =
+      inputtedNumber > randomNumber ? `📈 Too High! ` : `📉 Too Low!`;
+    currentScore--;
+    score.textContent = currentScore;
+    console.log(currentScore);
+  } else if (inputtedNumber === randomNumber && currentScore > 0) {
+    message.textContent = `🥇 Correct !`;
+    score.textContent = currentScore;
+    console.log(currentScore);
+  }
+
+  if (currentScore <= 0) {
+    message.textContent = `GAME OVER`;
+    currentScore = 0;
+    score.textContent = currentScore;
   }
 });
