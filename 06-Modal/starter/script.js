402c86064b87b5c704dad3aa70ad6a6d6303812e
DelanoRmdhn <del.191006@gmail.com>
2026-07-16 00:29:42 +0700

    1. MODAL PROJECT : querySelectorAll & how to treat querySelectorAll

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+/* 
+WEEK 3 DAY 12 : INTERAKSI MODAL WINDOW 
+
+gunakan document.querySelectorAll() apabila sebuah elemen memiliki sebuah class dengan nama yang sama dan elemen2 tersebut mau kita lakukan manipulasi.
+
+dia merupakan sebuah nodeList, dia mirip array tapi bukanlah sebuah array.
+
+*/
+
+//Simpan elemen kedalam sebuah variable
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('clicked!');
+  });
+}
