decbc30f982db887228dd8417075ed9f06445f4e
DelanoRmdhn <del.191006@gmail.com>
2026-07-16 01:42:39 +0700

    3. MODAL PROJECT FINISHED : Callback Function  & classList.contains

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -10,13 +10,41 @@ dia merupakan sebuah nodeList, dia mirip array tapi bukanlah sebuah array.
 //CARA MEMANIPULASI SEBUAH CLASS 
 kita bisa melakukan manipulasi baik itu menambah,menghapus,cek,dll sebuah class. kita bisa menggunakan classList.
 
+//CALLBACK FUNCTION : Callback adalah sebuah function yang dikirim sebagai argumen ke function lain, lalu function tersebut akan dipanggil oleh function penerima pada waktu yang tepat.
+
 */
+
+//contoh callback function :
+const delano = function (callback) {
+  let nama = 'delano';
+  console.log(`nama saya ${nama}, nama kamu siapa?`);
+
+  const a = callback(nama);
+  console.log(`Hai Juga ${a}!`);
+};
+
+const panggilIlham = function (namaKamu) {
+  let namaSaya = 'ilham';
+  console.log(`Halo ${namaKamu}, nama saya ${namaSaya}`);
+  return namaSaya;
+};
+
+delano(panggilIlham);
+
+//////////////////////////////////////////////////////////
 
 //Simpan elemen kedalam sebuah variable
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
 const btnCloseModal = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
+
+//Function Pengecekan keypress
+const checkKeyPressed = function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+};
 
 //Function tutup Modal
 const closeModal = function () {
@@ -36,6 +64,7 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 }
 
 //Tutup Modal dengan klik btnCloseModal , klik overlay, dan ketika user klik 'Escape'
-
+//PENERAPAN CALLBACK FUNCTION PADA STUDI KASUS MODAL
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', checkKeyPressed);
