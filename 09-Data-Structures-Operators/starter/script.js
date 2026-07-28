013cb5d2938dbece2af10fa093cfed6079b6fc63
Delano <del.191006@gmail.com>
2026-07-28 17:57:32 +0700

    2. Latihan Soal 2 : Destructuring Object

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -228,39 +228,78 @@ const books = [
 ];
 
 /* 
-Destructuring Arrays
+Destructuring Objects
+1. Destructure the first book object from the books array into variables called title, author and ISBN.
 
-1. Destructure the books array into two variables called firstBook and secondBook.
+2. Each book object has the keywords property. Destructure the first book object from the books array into a variable called tags. The tags variable should be assigned with the value of the keywords property.
 
-2.Destructure the books array into a variable called thirdBook. You must skip the first two books.
+3. The seventh book from the books array is missing the programmingLanguage property. Destructure the seventh book object (books[6]) into variables called language and programmingLanguage. Assign the programmingLanguage variable with a default value of 'unknown'.
 
-3.Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount. In the result of your destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.
+4. Below are two variables called bookTitle and bookAuthor. Reassign them with the values of the title and author properties of the first book object from the books array.
 
-const ratings = [['rating', 4.19], ['ratingsCount', 144584]]; 
+5. Each book object has a deeply nested rating property as illustrated below:
 
-4. Below is the ratingStars array. Destructure it into three variables called fiveStarRatings, oneStarRatings and threeStarRatings. Assign the threeStarRatings variable with a default value of 0.
+{
+  title: 'Algorithms',
+  ...
+  thirdParty: {
+    goodreads: {
+      rating: 4.41,              // <-- HERE
+      ratingsCount: 1733,
+      reviewsCount: 63,
+      fiveStarRatingCount: 976,
+      oneStarRatingCount: 13
+    }
+  }
+},
 
-const ratingStars = [63405, 1808];
+Destructure the first book object from the books array into a variable called bookRating. In the result of your destructuring, the bookRating variable should be assigned with the value of the book[0].thirdParty.goodreads.rating property.
 
+Please do most of the work on the left side of the assignment operator: const ... = books[0];
+
+6. Write a function called printBookInfo that has three parameters called title, author and year. This function should work for a single object passed as an argument, and it should log to the console information about the book in this format: "${title} by ${author}, ${year}".
+
+If year is undefined (was not passed), it should be assigned with a default value of 'year unknown'.
+
+printBookInfo({ title: 'Algorithms', author: 'Robert Sedgewick', year: '2011' });
 */
 
-//1
-const [firstBook, secondBook] = books;
-console.log(firstBook, secondBook);
+//1.
+const { title, author, ISBN } = books[0];
+console.log(title, author, ISBN);
 
 //2
-const [, , thirdBook] = books;
-console.log(thirdBook);
+const { keywords: tags } = books[0];
+console.log(tags);
 
 //3
-const ratings = [
-  ['rating', 4.19],
-  ['ratingsCount', 144584],
-];
-const [[, rating], [, ratingsCount]] = ratings;
-console.log(rating, ratingsCount);
+const { language, programmingLanguage = 'unknown' } = books[6];
+console.log(language, programmingLanguage);
 
 //4
-const ratingStars = [63405, 1808];
-const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
-console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
+let bookTitle = 'unknown';
+let bookAuthor = 'unknown';
+
+({ title: bookTitle, author: bookAuthor } = books[0]);
+console.log(bookTitle, bookAuthor);
+
+//5
+const {
+  thirdParty: {
+    goodreads: { rating: bookRating },
+  },
+} = books[0];
+console.log(bookRating);
+
+//6
+const printBookInfo = function ({ title, author, year = 'unknown' }) {
+  console.log(`${title} by ${author}, ${year}`);
+};
+
+printBookInfo({
+  title: 'Algorithms',
+  author: 'Robert Sedgewick',
+  year: '2011',
+});
+
+printBookInfo({ title: 'Algorithms', author: 'Robert Sedgewick' });
