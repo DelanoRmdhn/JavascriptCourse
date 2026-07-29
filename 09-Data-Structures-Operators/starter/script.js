2386f4fc9342acf0b86df02aa043c322f44859ba
Delano <del.191006@gmail.com>
2026-07-29 08:09:45 +0700

    Mini Task Object Destructuring,Nested Destructuring,Aliasing,Default Values :Sistem Ekstraksi Konfigurasi Server Agensi

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,284 +1,90 @@
 'use strict';
-const books = [
-  {
-    title: 'Algorithms',
-    author: ['Robert Sedgewick', 'Kevin Wayne'],
-    publisher: 'Addison-Wesley Professional',
-    publicationDate: '2011-03-24',
-    edition: 4,
-    keywords: [
-      'computer science',
-      'programming',
-      'algorithms',
-      'data structures',
-      'java',
-      'math',
-      'software',
-      'engineering',
-    ],
-    pages: 976,
-    format: 'hardcover',
-    ISBN: '9780321573513',
-    language: 'English',
-    programmingLanguage: 'Java',
-    onlineContent: true,
-    thirdParty: {
-      goodreads: {
-        rating: 4.41,
-        ratingsCount: 1733,
-        reviewsCount: 63,
-        fiveStarRatingCount: 976,
-        oneStarRatingCount: 13,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: 'Structure and Interpretation of Computer Programs',
-    author: [
-      'Harold Abelson',
-      'Gerald Jay Sussman',
-      'Julie Sussman (Contributor)',
-    ],
-    publisher: 'The MIT Press',
-    publicationDate: '2022-04-12',
-    edition: 2,
-    keywords: [
-      'computer science',
-      'programming',
-      'javascript',
-      'software',
-      'engineering',
-    ],
-    pages: 640,
-    format: 'paperback',
-    ISBN: '9780262543231',
-    language: 'English',
-    programmingLanguage: 'JavaScript',
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 4.36,
-        ratingsCount: 14,
-        reviewsCount: 3,
-        fiveStarRatingCount: 8,
-        oneStarRatingCount: 0,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: "Computer Systems: A Programmer's Perspective",
-    author: ['Randal E. Bryant', "David Richard O'Hallaron"],
-    publisher: 'Prentice Hall',
-    publicationDate: '2002-01-01',
-    edition: 1,
-    keywords: [
-      'computer science',
-      'computer systems',
-      'programming',
-      'software',
-      'C',
-      'engineering',
-    ],
-    pages: 978,
-    format: 'hardcover',
-    ISBN: '9780130340740',
-    language: 'English',
-    programmingLanguage: 'C',
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 4.44,
-        ratingsCount: 1010,
-        reviewsCount: 57,
-        fiveStarRatingCount: 638,
-        oneStarRatingCount: 16,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: 'Operating System Concepts',
-    author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
-    publisher: 'John Wiley & Sons',
-    publicationDate: '2004-12-14',
-    edition: 10,
-    keywords: [
-      'computer science',
-      'operating systems',
-      'programming',
-      'software',
-      'C',
-      'Java',
-      'engineering',
-    ],
-    pages: 921,
-    format: 'hardcover',
-    ISBN: '9780471694663',
-    language: 'English',
-    programmingLanguage: 'C, Java',
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 3.9,
-        ratingsCount: 2131,
-        reviewsCount: 114,
-        fiveStarRatingCount: 728,
-        oneStarRatingCount: 65,
-      },
-    },
-  },
-  {
-    title: 'Engineering Mathematics',
-    author: ['K.A. Stroud', 'Dexter J. Booth'],
-    publisher: 'Palgrave',
-    publicationDate: '2007-01-01',
-    edition: 14,
-    keywords: ['mathematics', 'engineering'],
-    pages: 1288,
-    format: 'paperback',
-    ISBN: '9781403942463',
-    language: 'English',
-    programmingLanguage: null,
-    onlineContent: true,
-    thirdParty: {
-      goodreads: {
-        rating: 4.35,
-        ratingsCount: 370,
-        reviewsCount: 18,
-        fiveStarRatingCount: 211,
-        oneStarRatingCount: 6,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: 'The Personal MBA: Master the Art of Business',
-    author: 'Josh Kaufman',
-    publisher: 'Portfolio',
-    publicationDate: '2010-12-30',
-    keywords: ['business'],
-    pages: 416,
-    format: 'hardcover',
-    ISBN: '9781591843528',
-    language: 'English',
-    thirdParty: {
-      goodreads: {
-        rating: 4.11,
-        ratingsCount: 40119,
-        reviewsCount: 1351,
-        fiveStarRatingCount: 18033,
-        oneStarRatingCount: 1090,
-      },
-    },
-  },
-  {
-    title: 'Crafting Interpreters',
-    author: 'Robert Nystrom',
-    publisher: 'Genever Benning',
-    publicationDate: '2021-07-28',
-    keywords: [
-      'computer science',
-      'compilers',
-      'engineering',
-      'interpreters',
-      'software',
-      'engineering',
-    ],
-    pages: 865,
-    format: 'paperback',
-    ISBN: '9780990582939',
-    language: 'English',
-    thirdParty: {
-      goodreads: {
-        rating: 4.7,
-        ratingsCount: 253,
-        reviewsCount: 23,
-        fiveStarRatingCount: 193,
-        oneStarRatingCount: 0,
-      },
-    },
-  },
-  {
-    title: 'Deep Work: Rules for Focused Success in a Distracted World',
-    author: 'Cal Newport',
-    publisher: 'Grand Central Publishing',
-    publicationDate: '2016-01-05',
-    edition: 1,
-    keywords: ['work', 'focus', 'personal development', 'business'],
-    pages: 296,
-    format: 'hardcover',
-    ISBN: '9781455586691',
-    language: 'English',
-    thirdParty: {
-      goodreads: {
-        rating: 4.19,
-        ratingsCount: 144584,
-        reviewsCount: 11598,
-        fiveStarRatingCount: 63405,
-        oneStarRatingCount: 1808,
-      },
-    },
-    highlighted: true,
-  },
-];
-
-//LATIHAN SPREAD
 /* 
-The Spread Operator﻿
-3.1﻿
-Each book object has the author property, which stores an array of strings (author names) if there are multiple authors, or a single string (author name) if there is just one author.
+MINI TASKS : 
 
-Declare an array called bookAuthors, and fill it with authors of the first two books from the books array. The bookAuthors array should have just one level (no nested arrays).
+SOAL PRAKTIKUM MANDIRI
+Topik: Object Destructuring, Nested Destructuring, Aliasing, & Default Values
 
-*/
-const bookAuthors = [...books[0].author, ...books[1].author];
-console.log(bookAuthors);
+Studi Kasus: Sistem Ekstraksi Konfigurasi Server Agensi
 
-/* 
-Write a function called spellWord that accepts a single string as an argument. This function should log to the console each letter of the argument separated by a space.
+Skenario
+Agensi PT. CAHAYA REMBULAN SEJATI sedang menyiapkan integrasi koneksi ke server API tiruan (mock API) untuk menampilkan data proyek klien. Pihak backend mengirimkan objek konfigurasi yang berisi detail server, kredensial, dan opsi timeout.
 
-Example
-Code:
+Anda diminta untuk mengambil beberapa data penting dari objek konfigurasi bersarang tersebut menggunakan satu baris sintaks Destructuring ES6 agar kodenya tetap bersih dan scannable.
 
-spellWord('JavaScript')
-Expected output:
+Instruksi Tugas
+Inisialisasi Data Objek Konfigurasi:
 
-"J a v a S c r i p t"
+Buat sebuah objek bernama apiConfig dengan struktur bersarang sebagai berikut:
+
+JavaScript
+const apiConfig = {
+  serviceName: "CahayaCRM Engine",
+  environment: "production",
+  connection: {
+    endpoint: {
+      baseURL: "https://api.cahayarembulan.com/v1",
+      port: 8080,
+    },
+    auth: {
+      apiKey: "CRS-SECRET-KEY-9981",
+    },
+  },
+};
+Ekstraksi Data Menggunakan Destructuring (Wajib 1 Deklarasi const):
+
+Lakukan destructuring pada objek apiConfig untuk mengambil data-data berikut sekaligus:
+
+serviceName (ambil langsung).
+
+baseURL yang berada di dalam connection -> endpoint (ubah nama variabelnya menjadi urlUtama menggunakan teknik aliasing).
+
+apiKey yang berada di dalam connection -> auth.
+
+timeout yang berada di dalam connection (karena properti timeout ini tidak ada di objek awal, berikan Default Value berupa angka 5000).
+
+Output Console:
+
+Cetak data-data hasil ekstraksi tersebut ke console browser menggunakan Template Literals dengan format pelaporan yang rapi.
+
+Target Tampilan Output Console:
+Plaintext
+=== API CONNECTION CONFIGURATION ===
+Nama Layanan : CahayaCRM Engine
+URL Target   : https://api.cahayarembulan.com/v1
+Secret Key   : CRS-SECRET-KEY-9981
+Max Timeout  : 5000 ms
 */
 
-const spellWord = function (word) {
-  console.log(...word);
+const apiConfig = {
+  serviceName: 'CahayaCRM Engine',
+  environment: 'production',
+  connection: {
+    endpoint: {
+      baseURL: 'https://api.cahayarembulan.com/v1',
+      port: 8080,
+    },
+    auth: {
+      apiKey: 'CRS-SECRET-KEY-9981',
+    },
+  },
 };
 
-spellWord('Javacript');
+const {
+  serviceName,
+  connection: {
+    endpoint: { baseURL: urlUtama },
+    auth: { apiKey },
+    timeout = 5000,
+  },
+} = apiConfig;
 
-//LATIHAN REST
-
-/* 
-Destructure the keywords property (array) of the first book from the books array into variables called mainKeyword and rest. The first keyword should be assigned to mainKeyword, and the rest of the keywords should be assigned to the rest variable (it should be an array).
-*/
-
-const [mainKeyword, ...rest] = books[0].keywords;
-console.log(mainKeyword, rest);
-
-/* 
-Destructure the second book from the books array into a variable called bookPublisher. The bookPublisher variable should be assigned with the value of the publisher property of the book object. Assign the rest of the properties to the restOfTheBook variable.
-
-*/
-
-const { publisher: bookPublisher, ...restOfTheBook } = books[1];
-console.log(bookPublisher, restOfTheBook);
-
-/* 
-Write a function called printBookAuthorsCount that has two parameters called title and authors. The authors parameter should accept any number of arguments. This function should log to the console a string formatted like that: "The book "${title}" has ${authors.length} authors".
-
-printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne');
-*/
-
-const printBookAuthorsCount = function (title, ...book) {
-  console.log(book);
-  console.log(`The Book ${title} has ${book.length}`);
-};
-printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne');
+console.log(
+  `
+  === API CONNECTION CONFIGURATION ===
+  Nama Layanan ${serviceName}
+  URL Target : ${urlUtama}
+  Secret Key : ${apiKey}
+  Max Timeout : ${timeout}
+  `,
+);
