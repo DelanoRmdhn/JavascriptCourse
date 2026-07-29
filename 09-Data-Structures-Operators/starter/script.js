28d5e4e7740027aa7225f5142c53fcc613b636d0
Delano <del.191006@gmail.com>
2026-07-29 08:43:31 +0700

    Latihan Soal Short Circuiting (AND OR) & Nullish Coalescing Operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,90 +1,284 @@
 'use strict';
-/* 
-MINI TASKS : 
 
-SOAL PRAKTIKUM MANDIRI
-Topik: Object Destructuring, Nested Destructuring, Aliasing, & Default Values
-
-Studi Kasus: Sistem Ekstraksi Konfigurasi Server Agensi
-
-Skenario
-Agensi PT. CAHAYA REMBULAN SEJATI sedang menyiapkan integrasi koneksi ke server API tiruan (mock API) untuk menampilkan data proyek klien. Pihak backend mengirimkan objek konfigurasi yang berisi detail server, kredensial, dan opsi timeout.
-
-Anda diminta untuk mengambil beberapa data penting dari objek konfigurasi bersarang tersebut menggunakan satu baris sintaks Destructuring ES6 agar kodenya tetap bersih dan scannable.
-
-Instruksi Tugas
-Inisialisasi Data Objek Konfigurasi:
-
-Buat sebuah objek bernama apiConfig dengan struktur bersarang sebagai berikut:
-
-JavaScript
-const apiConfig = {
-  serviceName: "CahayaCRM Engine",
-  environment: "production",
-  connection: {
-    endpoint: {
-      baseURL: "https://api.cahayarembulan.com/v1",
-      port: 8080,
+const books = [
+  {
+    title: 'Algorithms',
+    author: ['Robert Sedgewick', 'Kevin Wayne'],
+    publisher: 'Addison-Wesley Professional',
+    publicationDate: '2011-03-24',
+    edition: 4,
+    keywords: [
+      'computer science',
+      'programming',
+      'algorithms',
+      'data structures',
+      'java',
+      'math',
+      'software',
+      'engineering',
+    ],
+    pages: 976,
+    format: 'hardcover',
+    ISBN: '9780321573513',
+    language: 'English',
+    programmingLanguage: 'Java',
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.41,
+        ratingsCount: 1733,
+        reviewsCount: 63,
+        fiveStarRatingCount: 976,
+        oneStarRatingCount: 13,
+      },
     },
-    auth: {
-      apiKey: "CRS-SECRET-KEY-9981",
+    highlighted: true,
+  },
+  {
+    title: 'Structure and Interpretation of Computer Programs',
+    author: [
+      'Harold Abelson',
+      'Gerald Jay Sussman',
+      'Julie Sussman (Contributor)',
+    ],
+    publisher: 'The MIT Press',
+    publicationDate: '2022-04-12',
+    edition: 2,
+    keywords: [
+      'computer science',
+      'programming',
+      'javascript',
+      'software',
+      'engineering',
+    ],
+    pages: 640,
+    format: 'paperback',
+    ISBN: '9780262543231',
+    language: 'English',
+    programmingLanguage: 'JavaScript',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.36,
+        ratingsCount: 14,
+        reviewsCount: 3,
+        fiveStarRatingCount: 8,
+        oneStarRatingCount: 0,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Computer Systems: A Programmer's Perspective",
+    author: ['Randal E. Bryant', "David Richard O'Hallaron"],
+    publisher: 'Prentice Hall',
+    publicationDate: '2002-01-01',
+    edition: 1,
+    keywords: [
+      'computer science',
+      'computer systems',
+      'programming',
+      'software',
+      'C',
+      'engineering',
+    ],
+    pages: 978,
+    format: 'hardcover',
+    ISBN: '9780130340740',
+    language: 'English',
+    programmingLanguage: 'C',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 1010,
+        reviewsCount: 57,
+        fiveStarRatingCount: 638,
+        oneStarRatingCount: 16,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'Operating System Concepts',
+    author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
+    publisher: 'John Wiley & Sons',
+    publicationDate: '2004-12-14',
+    edition: 10,
+    keywords: [
+      'computer science',
+      'operating systems',
+      'programming',
+      'software',
+      'C',
+      'Java',
+      'engineering',
+    ],
+    pages: 921,
+    format: 'hardcover',
+    ISBN: '9780471694663',
+    language: 'English',
+    programmingLanguage: 'C, Java',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 3.9,
+        ratingsCount: 2131,
+        reviewsCount: 114,
+        fiveStarRatingCount: 728,
+        oneStarRatingCount: 65,
+      },
     },
   },
-};
-Ekstraksi Data Menggunakan Destructuring (Wajib 1 Deklarasi const):
+  {
+    title: 'Engineering Mathematics',
+    author: ['K.A. Stroud', 'Dexter J. Booth'],
+    publisher: 'Palgrave',
+    publicationDate: '2007-01-01',
+    edition: 14,
+    keywords: ['mathematics', 'engineering'],
+    pages: 1288,
+    format: 'paperback',
+    ISBN: '9781403942463',
+    language: 'English',
+    programmingLanguage: null,
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.35,
+        ratingsCount: 370,
+        reviewsCount: 18,
+        fiveStarRatingCount: 211,
+        oneStarRatingCount: 6,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'The Personal MBA: Master the Art of Business',
+    author: 'Josh Kaufman',
+    publisher: 'Portfolio',
+    publicationDate: '2010-12-30',
+    keywords: ['business'],
+    pages: 416,
+    format: 'hardcover',
+    ISBN: '9781591843528',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.11,
+        ratingsCount: 40119,
+        reviewsCount: 1351,
+        fiveStarRatingCount: 18033,
+        oneStarRatingCount: 1090,
+      },
+    },
+  },
+  {
+    title: 'Crafting Interpreters',
+    author: 'Robert Nystrom',
+    publisher: 'Genever Benning',
+    publicationDate: '2021-07-28',
+    keywords: [
+      'computer science',
+      'compilers',
+      'engineering',
+      'interpreters',
+      'software',
+      'engineering',
+    ],
+    pages: 865,
+    format: 'paperback',
+    ISBN: '9780990582939',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.7,
+        ratingsCount: 253,
+        reviewsCount: 23,
+        fiveStarRatingCount: 193,
+        oneStarRatingCount: 0,
+      },
+    },
+  },
+  {
+    title: 'Deep Work: Rules for Focused Success in a Distracted World',
+    author: 'Cal Newport',
+    publisher: 'Grand Central Publishing',
+    publicationDate: '2016-01-05',
+    edition: 1,
+    keywords: ['work', 'focus', 'personal development', 'business'],
+    pages: 296,
+    format: 'hardcover',
+    ISBN: '9781455586691',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.19,
+        ratingsCount: 144584,
+        reviewsCount: 11598,
+        fiveStarRatingCount: 63405,
+        oneStarRatingCount: 1808,
+      },
+    },
+    highlighted: true,
+  },
+];
 
-Lakukan destructuring pada objek apiConfig untuk mengambil data-data berikut sekaligus:
+//LATIHAN Short Circuiting (&& and ||)
 
-serviceName (ambil langsung).
+/* 
+Some of the book objects have the programmingLanguage property, which specifies what programming language is used in the book, for example
 
-baseURL yang berada di dalam connection -> endpoint (ubah nama variabelnya menjadi urlUtama menggunakan teknik aliasing).
+{
+  title: 'Algorithms',
+  author: ['Robert Sedgewick', 'Kevin Wayne'],
+  ...
+  programmingLanguage: 'Java',     // <-- HERE
+}
+Write a function called hasExamplesInJava that takes a book object from the books array as an argument. This function should return true if the book uses Java, or a string 'no data available' if it uses other language or no programming language at all.
 
-apiKey yang berada di dalam connection -> auth.
+Use short-circuiting.
 
-timeout yang berada di dalam connection (karena properti timeout ini tidak ada di objek awal, berikan Default Value berupa angka 5000).
+Example 1
+Example 2
+Code:
 
-Output Console:
+hasExamplesInJava(books[0]);
+Expected output:
 
-Cetak data-data hasil ekstraksi tersebut ke console browser menggunakan Template Literals dengan format pelaporan yang rapi.
-
-Target Tampilan Output Console:
-Plaintext
-=== API CONNECTION CONFIGURATION ===
-Nama Layanan : CahayaCRM Engine
-URL Target   : https://api.cahayarembulan.com/v1
-Secret Key   : CRS-SECRET-KEY-9981
-Max Timeout  : 5000 ms
+true
 */
 
-const apiConfig = {
-  serviceName: 'CahayaCRM Engine',
-  environment: 'production',
-  connection: {
-    endpoint: {
-      baseURL: 'https://api.cahayarembulan.com/v1',
-      port: 8080,
-    },
-    auth: {
-      apiKey: 'CRS-SECRET-KEY-9981',
-    },
-  },
+const hasExamplesInJava = function (book) {
+  return book.programmingLanguage === 'Java' || 'no data available';
 };
 
-const {
-  serviceName,
-  connection: {
-    endpoint: { baseURL: urlUtama },
-    auth: { apiKey },
-    timeout = 5000,
-  },
-} = apiConfig;
+console.log(hasExamplesInJava(books[0]));
+console.log(hasExamplesInJava(books[1]));
 
-console.log(
-  `
-  === API CONNECTION CONFIGURATION ===
-  Nama Layanan ${serviceName}
-  URL Target : ${urlUtama}
-  Secret Key : ${apiKey}
-  Max Timeout : ${timeout}
-  `,
-);
+/*
+Some of the book objects have the onlineContent property, which is either true or false. Loop over the books array, and for the books that provide online content, log to the console a string in this format: "${title}" provides online content. Use short-circuiting.
+
+{
+  title: 'Operating System Concepts',
+  // ... removed for clarity
+  onlineContent: false,          // <-- HERE
+},
+*/
+
+for (let i = 0; i < books.length; i++) {
+  console.log(
+    books[i].onlineContent && `${books[i].title} provides online content`,
+  );
+}
+
+//LATIHAN NULLISH COALESCING OPERATOR
+/*
+There are objects in the books array that don't have the onlineContent property at all. Loop over the books array, and log a string to the console in this format: "${title}" provides no data about its online content.
+*/
+
+for (let i = 0; i < books.length; i++) {
+  books[i].onlineContent ??
+    console.log(`${books[i].title} provides no data about its online content.`);
+}
