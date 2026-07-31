dcda40b3d28e1beb9652cda9e72223c4f73e73a7
Delano <del.191006@gmail.com>
2026-07-31 07:43:17 +0700

    1. CHALLANGE 1 : FOR LOOP

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,305 +1,137 @@
 'use strict';
 
-//LATIHAN DESTRUCTURING ARRAY
+//CHALLANGE 1 : FOR OF LOOP
+/*
+Challenge (Level Jonas Schmedtmann)
 
-const books = [
+Diberikan data berikut.
+
+const game = {
+  players: [
+    ["Neuer", "Pavard", "Davies"],
+    ["Burki", "Hummels", "Brandt"],
+  ],
+};
+
+Tanpa menggunakan for biasa, tampilkan
+
+Team 1
+Neuer
+Pavard
+Davies
+
+Team 2
+Burki
+Hummels
+Brandt
+*/
+
+const game = {
+  players: [
+    ['Neuer', 'Pavard', 'Davies'],
+    ['Burki', 'Hummels', 'Brandt'],
+  ],
+};
+
+for (const [i, player] of game.players.entries()) {
+  console.log(`Team${i + 1}`);
+  for (const playerName of player) {
+    console.log(playerName);
+  }
+  console.log('\n');
+}
+
+/*
+Misalkan state cart-mu seperti ini:
+
+const cart = [
   {
-    title: 'Algorithms',
-    author: ['Robert Sedgewick', 'Kevin Wayne'],
-    publisher: 'Addison-Wesley Professional',
-    publicationDate: '2011-03-24',
-    edition: 4,
-    keywords: [
-      'computer science',
-      'programming',
-      'algorithms',
-      'data structures',
-      'java',
-      'math',
-      'software',
-      'engineering',
-    ],
-    pages: 976,
-    format: 'hardcover',
-    ISBN: '9780321573513',
-    language: 'English',
-    programmingLanguage: 'Java',
-    onlineContent: true,
-    thirdParty: {
-      goodreads: {
-        rating: 4.41,
-        ratingsCount: 1733,
-        reviewsCount: 63,
-        fiveStarRatingCount: 976,
-        oneStarRatingCount: 13,
-      },
-    },
-    highlighted: true,
+    id: 1,
+    name: "Indomie",
+    price: 3500,
+    quantity: 2,
   },
   {
-    title: 'Structure and Interpretation of Computer Programs',
-    author: [
-      'Harold Abelson',
-      'Gerald Jay Sussman',
-      'Julie Sussman (Contributor)',
-    ],
-    publisher: 'The MIT Press',
-    publicationDate: '2022-04-12',
-    edition: 2,
-    keywords: [
-      'computer science',
-      'programming',
-      'javascript',
-      'software',
-      'engineering',
-    ],
-    pages: 640,
-    format: 'paperback',
-    ISBN: '9780262543231',
-    language: 'English',
-    programmingLanguage: 'JavaScript',
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 4.36,
-        ratingsCount: 14,
-        reviewsCount: 3,
-        fiveStarRatingCount: 8,
-        oneStarRatingCount: 0,
-      },
-    },
-    highlighted: true,
+    id: 2,
+    name: "Aqua",
+    price: 4000,
+    quantity: 1,
   },
   {
-    title: "Computer Systems: A Programmer's Perspective",
-    author: ['Randal E. Bryant', "David Richard O'Hallaron"],
-    publisher: 'Prentice Hall',
-    publicationDate: '2002-01-01',
-    edition: 1,
-    keywords: [
-      'computer science',
-      'computer systems',
-      'programming',
-      'software',
-      'C',
-      'engineering',
-    ],
-    pages: 978,
-    format: 'hardcover',
-    ISBN: '9780130340740',
-    language: 'English',
-    programmingLanguage: 'C',
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 4.44,
-        ratingsCount: 1010,
-        reviewsCount: 57,
-        fiveStarRatingCount: 638,
-        oneStarRatingCount: 16,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: 'Operating System Concepts',
-    author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
-    publisher: 'John Wiley & Sons',
-    publicationDate: '2004-12-14',
-    edition: 10,
-    keywords: [
-      'computer science',
-      'operating systems',
-      'programming',
-      'software',
-      'C',
-      'Java',
-      'engineering',
-    ],
-    pages: 921,
-    format: 'hardcover',
-    ISBN: '9780471694663',
-    language: 'English',
-    programmingLanguage: 'C, Java',
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 3.9,
-        ratingsCount: 2131,
-        reviewsCount: 114,
-        fiveStarRatingCount: 728,
-        oneStarRatingCount: 65,
-      },
-    },
-  },
-  {
-    title: 'Engineering Mathematics',
-    author: ['K.A. Stroud', 'Dexter J. Booth'],
-    publisher: 'Palgrave',
-    publicationDate: '2007-01-01',
-    edition: 14,
-    keywords: ['mathematics', 'engineering'],
-    pages: 1288,
-    format: 'paperback',
-    ISBN: '9781403942463',
-    language: 'English',
-    programmingLanguage: null,
-    onlineContent: true,
-    thirdParty: {
-      goodreads: {
-        rating: 4.35,
-        ratingsCount: 370,
-        reviewsCount: 18,
-        fiveStarRatingCount: 211,
-        oneStarRatingCount: 6,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: 'The Personal MBA: Master the Art of Business',
-    author: 'Josh Kaufman',
-    publisher: 'Portfolio',
-    publicationDate: '2010-12-30',
-    keywords: ['business'],
-    pages: 416,
-    format: 'hardcover',
-    ISBN: '9781591843528',
-    language: 'English',
-    thirdParty: {
-      goodreads: {
-        rating: 4.11,
-        ratingsCount: 40119,
-        reviewsCount: 1351,
-        fiveStarRatingCount: 18033,
-        oneStarRatingCount: 1090,
-      },
-    },
-  },
-  {
-    title: 'Crafting Interpreters',
-    author: 'Robert Nystrom',
-    publisher: 'Genever Benning',
-    publicationDate: '2021-07-28',
-    keywords: [
-      'computer science',
-      'compilers',
-      'engineering',
-      'interpreters',
-      'software',
-      'engineering',
-    ],
-    pages: 865,
-    format: 'paperback',
-    ISBN: '9780990582939',
-    language: 'English',
-    thirdParty: {
-      goodreads: {
-        rating: 4.7,
-        ratingsCount: 253,
-        reviewsCount: 23,
-        fiveStarRatingCount: 193,
-        oneStarRatingCount: 0,
-      },
-    },
-  },
-  {
-    title: 'Deep Work: Rules for Focused Success in a Distracted World',
-    author: 'Cal Newport',
-    publisher: 'Grand Central Publishing',
-    publicationDate: '2016-01-05',
-    edition: 1,
-    keywords: ['work', 'focus', 'personal development', 'business'],
-    pages: 296,
-    format: 'hardcover',
-    ISBN: '9781455586691',
-    language: 'English',
-    thirdParty: {
-      goodreads: {
-        rating: 4.19,
-        ratingsCount: 144584,
-        reviewsCount: 11598,
-        fiveStarRatingCount: 63405,
-        oneStarRatingCount: 1808,
-      },
-    },
-    highlighted: true,
+    id: 3,
+    name: "Teh Botol",
+    price: 5000,
+    quantity: 4,
   },
 ];
 
-/* 
-Destructuring Objects
-1. Destructure the first book object from the books array into variables called title, author and ISBN.
+Gunakan hanya for...of (ditambah destructuring jika perlu) untuk menghitung:
 
-2. Each book object has the keywords property. Destructure the first book object from the books array into a variable called tags. The tags variable should be assigned with the value of the keywords property.
+Total quantity seluruh barang.
+Subtotal belanja.
+Pajak 10%.
+Grand total.
+Cetak receipt seperti berikut:
+===== RECEIPT =====
 
-3. The seventh book from the books array is missing the programmingLanguage property. Destructure the seventh book object (books[6]) into variables called language and programmingLanguage. Assign the programmingLanguage variable with a default value of 'unknown'.
+Indomie
+2 x Rp3500 = Rp7000
 
-4. Below are two variables called bookTitle and bookAuthor. Reassign them with the values of the title and author properties of the first book object from the books array.
+Aqua
+1 x Rp4000 = Rp4000
 
-5. Each book object has a deeply nested rating property as illustrated below:
+Teh Botol
+4 x Rp5000 = Rp20000
 
-{
-  title: 'Algorithms',
-  ...
-  thirdParty: {
-    goodreads: {
-      rating: 4.41,              // <-- HERE
-      ratingsCount: 1733,
-      reviewsCount: 63,
-      fiveStarRatingCount: 976,
-      oneStarRatingCount: 13
-    }
-  }
-},
-
-Destructure the first book object from the books array into a variable called bookRating. In the result of your destructuring, the bookRating variable should be assigned with the value of the book[0].thirdParty.goodreads.rating property.
-
-Please do most of the work on the left side of the assignment operator: const ... = books[0];
-
-6. Write a function called printBookInfo that has three parameters called title, author and year. This function should work for a single object passed as an argument, and it should log to the console information about the book in this format: "${title} by ${author}, ${year}".
-
-If year is undefined (was not passed), it should be assigned with a default value of 'year unknown'.
-
-printBookInfo({ title: 'Algorithms', author: 'Robert Sedgewick', year: '2011' });
+--------------------
+Subtotal : Rp31000
+Tax      : Rp3100
+Total    : Rp34100
 */
-
-//1.
-const { title, author, ISBN } = books[0];
-console.log(title, author, ISBN);
-
-//2
-const { keywords: tags } = books[0];
-console.log(tags);
-
-//3
-const { language, programmingLanguage = 'unknown' } = books[6];
-console.log(language, programmingLanguage);
-
-//4
-let bookTitle = 'unknown';
-let bookAuthor = 'unknown';
-
-({ title: bookTitle, author: bookAuthor } = books[0]);
-console.log(bookTitle, bookAuthor);
-
-//5
-const {
-  thirdParty: {
-    goodreads: { rating: bookRating },
+const cart = [
+  {
+    id: 1,
+    name: 'Indomie',
+    price: 3500,
+    quantity: 2,
   },
-} = books[0];
-console.log(bookRating);
+  {
+    id: 2,
+    name: 'Aqua',
+    price: 4000,
+    quantity: 1,
+  },
+  {
+    id: 3,
+    name: 'Teh Botol',
+    price: 5000,
+    quantity: 4,
+  },
+];
 
-//6
-const printBookInfo = function ({ title, author, year = 'unknown' }) {
-  console.log(`${title} by ${author}, ${year}`);
+const generateReceipt = function () {
+  console.log('===== RECEIPT =====');
+
+  let subtotal = 0;
+
+  for (const { name, price, quantity } of cart) {
+    const itemTotal = price * quantity;
+
+    console.log(`
+${name}
+${quantity} x Rp${price} = Rp${itemTotal}
+`);
+
+    subtotal += itemTotal;
+  }
+
+  const tax = subtotal * 0.1;
+  const total = subtotal + tax;
+
+  console.log(`
+Subtotal : Rp${subtotal}
+Tax      : Rp${tax}
+Total    : Rp${total}
+`);
 };
-
-printBookInfo({
-  title: 'Algorithms',
-  author: 'Robert Sedgewick',
-  year: '2011',
-});
-
-printBookInfo({ title: 'Algorithms', author: 'Robert Sedgewick' });
+generateReceipt();
