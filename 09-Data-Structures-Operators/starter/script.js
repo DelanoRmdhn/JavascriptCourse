4422a3992ee47b506335d59c78528c59a75f83f2
Delano <del.191006@gmail.com>
2026-08-01 08:05:48 +0700

    3. Looping an Object : Object.keys, Object.values, Object.entries

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -16,67 +16,66 @@ const openingHours = {
   },
 };
 
-const restaurant = [
-  {
-    name: 'Classico Italiano',
-    location: 'Via Angelo Tavanti 23, Firenze, Italy',
-    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-    // ES6 enhanced object literals
-    openingHours,
+  // ES6 enhanced object literals
+  openingHours,
 
-    order(starterIndex, mainIndex) {
-      return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-    },
-
-    orderDelivery({
-      starterIndex = 1,
-      mainIndex = 0,
-      time = '20:00',
-      address,
-    }) {
-      console.log(
-        `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
-      );
-    },
-
-    orderPasta(ing1, ing2, ing3) {
-      console.log(
-        `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`,
-      );
-    },
-
-    orderPizza(mainIngredient, ...otherIngredients) {
-      console.log(mainIngredient);
-      console.log(otherIngredients);
-    },
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-];
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`,
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
 
 /*
-OPTIONAL CHAINING ?. : Operator yang digunakan untuk mengakses properti atau memanggil method dalam sebuah object dengan aman. jika suatu properti nilainya adalah undefined / null, kita bisa gunakan optional chaining sebagai cara dalam melakukan pengecekan apakah properti tersebut ada / tidak nilainya.
 
-operator ini seringnya digunakan untuk melakukan pengecekan properti / method pada object bersarang (nested object).
-*/
+  Looping Sebuah Object : dalam mengakses object kita bisa menggunakan for of loop, namun untuk mengakses object kita harus menggunakan Object.keys(), Object.values(), Object.entries() dalam mengakses object tersebut. 
+  Object.keys() digunakan untuk mengakses key dari object, 
+  Object.values() digunakan untuk mengakses value dari object, 
+  dan Object.entries() digunakan untuk mengakses key dan value dari object.
+  */
 
-console.log(restaurant[0].openingHours?.fri);
-
-/*
-?. setelah openingHours berarti apakah properti openingHours pada object restaurant[0] ada? jika ada jalankan properti fri, jika tidak kmbalikan undefined 
-*/
-
-//penggunaan ?. pada method
-restaurant[0].orderPizza?.('ikan', 'brokoli', 'jamur'); //-> berarti jika restaurant[0] memiliki method orderPizza, jalankan method tersebut
-
-const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-for (const day of days) {
-  const open = restaurant[0].openingHours[day]?.open ?? 10;
-
-  if (open !== undefined) {
-    console.log(`On ${day} we're open at ${open}`);
-  } else {
-    console.log(`We are closed on ${day}`);
+const getKeys = function (obj) {
+  for (const day of Object.keys(obj)) {
+    console.log(`Hari Buka Restoran : ${day}`);
   }
-}
+};
+
+const getValues = function (obj) {
+  for (const jamBuka of Object.values(openingHours)) {
+    console.log(`Jam Buka Restoran : ${jamBuka.open} - ${jamBuka.close}`);
+  }
+};
+
+const getEntries = function (obj) {
+  for (const [day, { open, close }] of Object.entries(openingHours)) {
+    console.log(`Hari Buka Restoran : ${day} Jam Buka : ${open} - ${close}`);
+  }
+};
+
+// getKeys(openingHours);
+// getValues(openingHours);
+// getEntries(openingHours);
+
+////////////////////////////////
