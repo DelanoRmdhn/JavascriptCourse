6cff219784bf9e45da0c0b11ed0a37954101f4f3
Delano <del.191006@gmail.com>
2026-08-02 05:58:49 +0700

    EXERCISE 2 : FOR LOOP, ENHANCED OBJECT LITERALS, LOOPING OBJECT #2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,67 +1,71 @@
 'use strict';
 
-'use strict';
+const store = {
+  name: 'Delano Store',
 
-const company = {
-  name: 'TechNova',
+  products: [
+    {
+      name: 'Keyboard',
+      category: 'Electronics',
+      sold: 23,
+    },
 
-  employees: [
     {
-      name: 'Delano',
-      department: 'Engineering',
-      tasksCompleted: 18,
+      name: 'Mouse',
+      category: 'Electronics',
+      sold: 15,
     },
+
     {
-      name: 'Rangga',
-      department: 'Design',
-      tasksCompleted: 12,
+      name: 'Notebook',
+      category: 'Stationery',
+      sold: 30,
     },
+
     {
-      name: 'Fadhil',
-      department: 'Engineering',
-      tasksCompleted: 20,
-    },
-    {
-      name: 'Alya',
-      department: 'Marketing',
-      tasksCompleted: 15,
+      name: 'Pen',
+      category: 'Stationery',
+      sold: 40,
     },
   ],
 
-  performance: {
-    Engineering: 9.2,
-    Design: 8.4,
-    Marketing: 7.8,
+  ratings: {
+    service: 4.8,
+    delivery: 4.5,
+    quality: 4.9,
   },
 };
 
-for (const [i, { name }] of Object.entries(company.employees)) {
-  console.log(`Employee ${Number(i) + 1} : ${name}`);
+for (const [i, { name }] of store.products.entries()) {
+  console.log(`Product ${i + 1} : ${name}`);
+}
+let totalProdukTerjual = 0;
+for (const { sold } of store.products) {
+  totalProdukTerjual += sold;
+}
+console.log(`Total Produk Terjual : ${totalProdukTerjual}`);
+
+let totalRating = 0;
+for (const rating of Object.values(store.ratings)) {
+  totalRating += rating;
+}
+const avgRating = totalRating / Object.values(store.ratings).length;
+console.log(`Average Rating : ${avgRating}`);
+
+for (const [i, values] of Object.entries(store.ratings)) {
+  console.log(`Rating for ${i} : ${values}`);
 }
 
-let totalTaskCompleted = 0;
-for (const [i, { tasksCompleted }] of Object.entries(company.employees)) {
-  totalTaskCompleted += tasksCompleted;
-}
-console.log(totalTaskCompleted);
+const saleBasedOnCategory = {};
 
-let totalPerformace = 0;
-for (const performance of Object.values(company.performance)) {
-  totalPerformace += performance;
-}
-totalPerformace = totalPerformace / Object.keys(company.performance).length;
-console.log(totalPerformace);
+for (const { category, sold } of store.products) {
+  // console.log(category, sold);
 
-for (const [division, score] of Object.entries(company.performance))
-  console.log(`Performance of ${division} : ${score}`);
-
-const departmentCount = {};
-
-for (const { department } of company.employees) {
-  if (departmentCount[department]) {
-    departmentCount[department]++;
+  if (saleBasedOnCategory[category]) {
+    saleBasedOnCategory[category] += sold;
   } else {
-    departmentCount[department] = 1;
+    saleBasedOnCategory[category] = sold;
   }
 }
-console.log(departmentCount);
+
+console.log(saleBasedOnCategory);
