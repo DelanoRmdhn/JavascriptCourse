ab21f0d88965a491226b5fca0ad61137a5c1cce0
Delano <del.191006@gmail.com>
2026-08-02 07:41:16 +0700

    EXERCISE 4 : FOR OF, OBJECT.ENTRIES, OBJECT.VALUES, DESTRUCTURING, NESTED OBJECT, ENHANCED OBJECT LITERALS, FREQUENCY COUNTER, MAXIMUM FINDER

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,70 +1,115 @@
 'use strict';
 
-const league = {
-  teams: {
-    arsenal: 'Arsenal',
-    city: 'Manchester City',
-    liverpool: 'Liverpool',
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri'];
+
+const schedule = {
+  [weekdays[0]]: {
+    topic: 'Arrays',
+    hours: 2,
   },
 
-  matches: [
-    {
-      home: 'arsenal',
-      away: 'city',
-      score: '2:1',
-      scorers: ['Saka', 'Haaland', 'Martinelli'],
-    },
+  [weekdays[1]]: {
+    topic: 'Objects',
+    hours: 3,
+  },
 
-    {
-      home: 'city',
-      away: 'liverpool',
-      score: '3:2',
-      scorers: ['Haaland', 'Haaland', 'Salah', 'Foden', 'Nunez'],
-    },
-  ],
+  [weekdays[2]]: {
+    topic: 'Destructuring',
+    hours: 2,
+  },
+
+  [weekdays[3]]: {
+    topic: 'Spread',
+    hours: 2,
+  },
+
+  [weekdays[4]]: {
+    topic: 'Rest',
+    hours: 1,
+  },
 };
 
-for (const [i, { home, away }] of league.matches.entries()) {
-  console.log(`Match ${i + 1} : `);
-  console.log(`${league.teams[home]} VS ${league.teams[away]}`);
-}
+const student = {
+  name: 'Delano',
 
-let goalNumber = 1;
-for (const [key, { scorers }] of league.matches.entries()) {
-  console.log(`Match ${key + 1} : `);
-  for (const name of scorers) {
-    console.log(`Goal ${goalNumber} : ${name}`);
-    goalNumber++;
+  completedTopics: ['Arrays', 'Objects', 'Objects', 'Spread', 'Arrays', 'Rest'],
+
+  schedule,
+};
+
+const getSchedule = function () {
+  for (const [day, { topic, hours }] of Object.entries(student.schedule)) {
+    console.log(`Day : ${day}`);
+    console.log(`Topic : ${topic}`);
+    console.log(`Hours : ${hours}`);
   }
-}
+};
 
-const goalScorer = {};
-for (const { scorers } of league.matches) {
-  for (const player of scorers) {
-    if (goalScorer[player]) {
-      goalScorer[player] += 1;
+// getSchedule();
+
+const getTotalStudyHour = function () {
+  let totalStudyHour = 0;
+  for (const { hours } of Object.values(student.schedule)) {
+    totalStudyHour += hours;
+  }
+  console.log(`Total Study Hours : ${totalStudyHour}`);
+};
+
+// getTotalStudyHour();
+
+const getAverageStudyHours = function () {
+  let totalStudyHour = 0;
+  for (const { hours } of Object.values(student.schedule)) {
+    totalStudyHour += hours;
+  }
+  console.log(`Average Study Hours : ${totalStudyHour / weekdays.length}`);
+};
+
+// getAverageStudyHours();
+
+const getTopicFinished = function () {
+  const topicFinished = {};
+
+  for (const topic of student.completedTopics) {
+    if (topicFinished[topic]) {
+      topicFinished[topic] += 1;
     } else {
-      goalScorer[player] = 1;
+      topicFinished[topic] = 1;
     }
   }
-}
+  return topicFinished;
+};
+const topicFinished = getTopicFinished();
+console.log(topicFinished);
 
-console.log(goalScorer);
-let totalGoals = 0;
-for (const { scorers } of league.matches) {
-  totalGoals += scorers.length;
-}
-console.log(totalGoals);
-
-let topScorer = '';
-let maxGoals = 0;
-
-for (const [name, score] of Object.entries(goalScorer)) {
-  console.log(name, score);
-  if (score > maxGoals) {
-    topScorer = name;
-    maxGoals = score;
+const getStudyInformation = function () {
+  for (const [day, { topic, hours }] of Object.entries(student.schedule)) {
+    console.log(`On ${day} study ${topic} for ${hours} hours.`);
   }
-}
+};
 
-console.log(`Top Scorer adalah ${topScorer} dengan ${maxGoals} Gol.`);
+// getStudyInformation();
+const mostCompletedTopic = function () {
+  let topic = '';
+  let maxCompleted = 0;
+  let mostCompletedTopic = [];
+
+  for (const [key, value] of Object.entries(topicFinished)) {
+    console.log(key, value);
+    if (value > maxCompleted) {
+      mostCompletedTopic = [];
+      topic = key;
+      maxCompleted = value;
+      mostCompletedTopic.push(key);
+    } else if (value === maxCompleted) {
+      topic = key;
+      maxCompleted = value;
+      mostCompletedTopic.push(key);
+    }
+  }
+  console.log(`Most Completed Topics`);
+  for (const topic of mostCompletedTopic) {
+    console.log(topic);
+  }
+};
+mostCompletedTopic();
