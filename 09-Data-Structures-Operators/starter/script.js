81f0042febdb3b3621b16495afd5a4abd99aa2ef
Delano <del.191006@gmail.com>
2026-08-03 06:29:44 +0700

    6. Another Method of Sets : intersection,union,difference,symmetricDifference,and isDisjointFrom

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,87 +1,40 @@
 'use strict';
 
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-const openingHours = {
-  [weekdays[3]]: {
-    open: 12,
-    close: 22,
-  },
-  [weekdays[4]]: {
-    open: 11,
-    close: 23,
-  },
-  [weekdays[5]]: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-};
+const italianFoods = new Set([
+  'pasta',
+  'gnocchi',
+  'tomatoes',
+  'olive oil',
+  'garlic',
+  'basil',
+]);
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+const mexicanFoods = new Set([
+  'tortillas',
+  'beans',
+  'rice',
+  'tomatoes',
+  'avocado',
+  'garlic',
+]);
 
-  // ES6 enhanced object literals
-  openingHours,
-};
+const commonFoods = italianFoods.intersection(mexicanFoods);
+console.log('Intersection:', commonFoods);
+console.log([...commonFoods]);
 
-/*
-Challenge 1 — Weekly Opening Report (⭐⭐⭐⭐)
+const italianMexicanFusion = italianFoods.union(mexicanFoods);
+console.log('Union:', italianMexicanFusion);
 
-Buat program yang menghasilkan output berikut menggunakan object restaurant di atas.
+console.log([...new Set([...italianFoods, ...mexicanFoods])]);
 
-Output:
+const uniqueItalianFoods = italianFoods.difference(mexicanFoods);
+console.log('Difference italian:', uniqueItalianFoods);
 
-===== WEEKLY OPENING REPORT =====
+const uniqueMexicanFoods = mexicanFoods.difference(italianFoods);
+console.log('Difference mexican:', uniqueMexicanFoods);
 
-THU : Open at 12, Close at 22
-FRI : Open at 11, Close at 23
-SAT : Open 24 Hours
-*/
+const uniqueItalianAndMexicanFoods =
+  italianFoods.symmetricDifference(mexicanFoods);
+console.log(uniqueItalianAndMexicanFoods);
 
-//SETS : Struktur data yang digunakan untuk menyimpan sebuah iterable (array) dengan nilai unik.
-
-const orderFood = [
-  'Ayam',
-  'Ikan',
-  'Sapi',
-  'sapi',
-  'Ayam',
-  'Sapi',
-  'Jeruk Bali',
-];
-
-const getOrderFood = new Set(orderFood);
-console.log(getOrderFood);
-
-/* getOrderFood beriisikan dengan nilai nilai dari elemen orderFood dengan nilai unik. kita tidak bisa mengeluarkan nilai yang ada didalam sets dengan cara mengakses indexnya. tetapi kita bisa mengkonversinya menjadi sebuah array dengan spread operator dengan cara dibawah ini :
- */
-
-// const getAllOrderFood = [...getOrderFood];
-// console.log(getAllOrderFood);
-
-/*
-atau kita bisa langsung mengkonversi hasil sets tersebut ke array
-
-const getOrderFood = [...new Set(orderFood)];
-console.log(getOrderFood);
-
-*/
-
-//Method Method pada sets
-
-//1. add()
-getOrderFood.add('Babi');
-console.log(getOrderFood);
-
-//2. delete()
-getOrderFood.delete('sapi');
-console.log(getOrderFood);
-
-//3. has()
-console.log(getOrderFood.has('Ikan'));
-
-//4. size
-console.log(getOrderFood.size);
+console.log(italianFoods.isDisjointFrom(mexicanFoods));
