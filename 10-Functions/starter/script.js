3539c34f343449ab7c0d4c8d27cc7fd3d7918c93
Delano <del.191006@gmail.com>
2026-08-05 18:47:33 +0700

    2. Cara Kerja Pass in Reference Value & Object Value as an Argument in Function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -26,3 +26,43 @@ createBooking('Sriwijaya Air', undefined, 60);
 createBooking(undefined, undefined, 30);
 
 console.log(bookings);
+
+const flight = 'GI112';
+const delano = {
+  nama: 'Delano Bariq Ramadhan',
+  noPaspor: 93021848311,
+};
+
+const checkin = function (flightNumber, passanggers) {
+  flightNumber = 'GI113';
+  passanggers.nama = 'Mr. Delano Bariq Ramadhan';
+
+  if (passanggers.noPaspor === 93021848311) {
+    console.log(`${passanggers.nama} Berhasil Check In!`);
+  } else {
+    console.log(`${passanggers.nama} Nomor Pasport Kamu Salah!`);
+  }
+};
+
+checkin(flight, delano);
+console.log(flight);
+console.log(delano);
+
+const checkInWithoutChangingObject = function (flightNumber, passanggers) {
+  flightNumber = 'GI104';
+  const passangger = { ...passanggers };
+
+  if (
+    passangger.noPaspor === 93021848311 &&
+    passangger.nama === 'Mr. Delano Bariq Ramadhan'
+  ) {
+    passangger.nama = 'Mr. Delano Bariq Ramadhan';
+    alert(`${passangger.nama} Successfully Checked In!`);
+  } else {
+    alert(`❌ Invalid Name / Pasport Number!`);
+  }
+};
+
+checkInWithoutChangingObject(flight, delano);
+console.log(flight);
+console.log(delano);
