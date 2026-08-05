030b9d69c4be3eb9457a2b8988b72035448dd23b
Delano <del.191006@gmail.com>
2026-08-05 16:37:07 +0700

    1. Default Parameters in Function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+//1.DEFAULT PARAMETES : sebuah cara untuk menyetel nila default sebuah argumen pada function.
+
+const bookings = [];
+
+const createBooking = function (
+  plane = 'Batik Air',
+  passanggers = 10,
+  price = 50,
+) {
+  const grossProfit = passanggers * price;
+
+  const booking = {
+    plane,
+    passanggers,
+    price,
+    grossProfit,
+  };
+  bookings.push(booking);
+};
+
+createBooking('Etihad Airways');
+createBooking('Garuda Indonesia', 100, 120);
+createBooking('Sriwijaya Air', undefined, 60);
+createBooking(undefined, undefined, 30);
+
+console.log(bookings);
