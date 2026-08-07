1466d29d77c01dadc04107d8541f20219f9a0a92
Delano <del.191006@gmail.com>
2026-08-07 16:58:24 +0700

    3. Higher Order Function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,68 +1,40 @@
 'use strict';
 
-//1.DEFAULT PARAMETES : sebuah cara untuk menyetel nila default sebuah argumen pada function.
-
-const bookings = [];
-
-const createBooking = function (
-  plane = 'Batik Air',
-  passanggers = 10,
-  price = 50,
-) {
-  const grossProfit = passanggers * price;
-
-  const booking = {
-    plane,
-    passanggers,
-    price,
-    grossProfit,
-  };
-  bookings.push(booking);
+//2. Higher Order Function
+const sopDasar = function () {
+  console.log(`1. Cuci Tangan Menggunakan Sabun`);
+  console.log(`2. Pakai Masker dan Pelindung Kepala`);
+  console.log(`3. Memakai Kacamata Pelindung`);
 };
 
-createBooking('Etihad Airways');
-createBooking('Garuda Indonesia', 100, 120);
-createBooking('Sriwijaya Air', undefined, 60);
-createBooking(undefined, undefined, 30);
-
-console.log(bookings);
-
-const flight = 'GI112';
-const delano = {
-  nama: 'Delano Bariq Ramadhan',
-  noPaspor: 93021848311,
+const masakPizza = function () {
+  console.log('Pizza Sedang dimasak');
 };
 
-const checkin = function (flightNumber, passanggers) {
-  flightNumber = 'GI113';
-  passanggers.nama = 'Mr. Delano Bariq Ramadhan';
+const masakBurger = function () {
+  console.log('Burger Sedang dimasak');
+};
 
-  if (passanggers.noPaspor === 93021848311) {
-    console.log(`${passanggers.nama} Berhasil Check In!`);
-  } else {
-    console.log(`${passanggers.nama} Nomor Pasport Kamu Salah!`);
+const masakMakanan = function (jenisMasakan) {
+  sopDasar();
+  jenisMasakan();
+};
+
+masakMakanan(masakBurger);
+masakMakanan(masakPizza);
+
+//Contoh Lainnya
+const foods = ['Sop Sapi', 'Ayam bakar'];
+const masakApa = function (jenisMakanan) {
+  for (const makanan of jenisMakanan) {
+    console.log(`${makanan} Sedang Dibuat!`);
   }
 };
 
-checkin(flight, delano);
-console.log(flight);
-console.log(delano);
-
-const checkInWithoutChangingObject = function (flightNumber, passanggers) {
-  flightNumber = 'GI104';
-  const passangger = { ...passanggers };
-
-  if (
-    passangger.noPaspor === 93021848311 &&
-    passangger.nama === 'Mr. Delano Bariq Ramadhan'
-  ) {
-    passangger.nama = 'Mr. Delano Bariq Ramadhan';
-    alert(`${passangger.nama} Successfully Checked In!`);
-  } else {
-    alert(`❌ Invalid Name / Pasport Number!`);
-  }
+const mariMasak = function (makanan, fn) {
+  console.log(`Cuci Tangan`);
+  console.log(`Siapkan Peralatan Memasak`);
+  fn(makanan);
 };
 
-checkInWithoutChangingObject(flight, delano);
-console.log(flight);
-console.log(delano);
+mariMasak(foods, masakApa);
