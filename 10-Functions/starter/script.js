1c817c8b6f3002d4ec9f9b68c2fa8845551ed1c0
Delano <del.191006@gmail.com>
2026-08-10 22:14:56 +0700

    Mini Task : Buat higher-order function bernama modifierHarga dengan callback khusus

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,98 +1,103 @@
 'use strict';
 
 /*
-Let's build a simple poll app!
-A poll has a question, an array of options from which people can choose, and an
-array with the number of replies for each option. This data is stored in the starter
-'poll' object below.
 
-Your tasks:
-1. Create a method called 'registerNewAnswer' on the 'poll' object. The
-method does 2 things:
+Nama Task: Implementation of Higher-Order Function modifierHarga
 
-1.1. Display a prompt window for the user to input the number of the
-selected option. The prompt should look like this:
-What is your favourite programming language?
-0: JavaScript
-1: Python
-2: Rust
-3: C++
+Topik: Functional Programming, Higher-Order Functions (HOF), Array .map(), & Callback Functions
 
-(Write option number)
+Studi Kasus: Modifikasi Massal Harga Kontrak Proyek Agensi
 
-1.2. Based on the input number, update the 'answers' array property. For
-example, if the option is 3, increase the value at position 3 of the array by
+1. Skenario Bisnis
+Tim Finansial dan Tim Sales dari PT. CAHAYA REMBULAN SEJATI sering kali perlu menyesuaikan nilai kontrak beberapa proyek sekaligus berdasarkan kondisi tertentu (misalnya penerapan promo diskon musiman, perhitungan PPN internal, atau sekadar memformat tampilan angka murni ke format mata uang Rupiah).
 
-1. Make sure to check if the input is a number and if the number makes
-sense (e.g. answer 52 wouldn't make sense, right?)
+Untuk menghindari duplikasi kode (Don't Repeat Yourself / DRY), kamu diminta membuat sebuah Higher-Order Function (HOF) serbaguna bernama modifierHarga. Fungsi ini akan bertindak sebagai mesin pemproses utama yang menerima kumpulan harga proyek dan menerapkan aturan transformasi harga sesuai dengan fungsi callback yang dikirimkan.
 
-2. Call this method whenever the user clicks the "Answer poll" button.
+2. Fitur & Instruksi Tugas
+Buat Higher-Order Function (modifierHarga):
 
-3. Create a method 'displayResults' which displays the poll results. The
-method takes a string as an input (called 'type'), which can be either 'string'
-or 'array'. If type is 'array', simply display the results array as it is, using
-console.log(). This should be the default option. If type is 'string', display a
-string like "Poll results are 13, 2, 4, 1".
+Buat fungsi modifierHarga yang menerima 2 parameter:
 
-4. Run the 'displayResults' method at the end of each
-'registerNewAnswer' method call.
+daftarHarga (Array berisi daftar angka harga murni).
 
-5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test
-data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll
-object! So what should the this keyword look like in this situation?
+callback (Fungsi yang bertindak sebagai transformer nilai tiap item).
 
-Test data for bonus:
-§ Data 1: [5, 2, 3]
-§ Data 2: [1, 5, 3, 9, 6, 1]
+Di dalam modifierHarga, manfaatkan array method bawaan JavaScript (seperti .map()) untuk memproses setiap harga murni dengan callback tersebut, lalu return array hasil operasinya.
 
-Hints: Use many of the tools you learned about in this and the last section 😉
-GOOD LUCK 😀
+Buat 3 Fungsi Callback Khusus:
+Buat 3 fungsi callback terpisah yang akan dilempar ke dalam modifierHarga:
+
+diskonKlienSetia: Mengalikan harga dengan 0.80 (potongan diskon 20%).
+
+tambahPajakPPN: Mengalikan harga dengan 1.11 (penambahan PPN 11%).
+
+formatKeRupiah: Mengubah angka murni menjadi string berformat Rupiah menggunakan .toLocaleString('id-ID') (contoh: 5000000 menjadi "Rp 5.000.000").
+
+Inisialisasi Data & Eksekusi:
+
+Deklarasikan array hargaAwal berisi nilai: [3000000, 5000000, 8000000].
+
+Jalankan modifierHarga sebanyak 3x menggunakan masing-masing callback di atas secara bergantian dan simpan hasilnya di variabel terpisah.
+
+Output Console:
+
+Cetak seluruh log proses transformasi harga tersebut ke console browser dengan Template Literals yang rapi.
+
+3. Target Tampilan Output Console:
+Plaintext
+=== FINANCIAL PRICE MODIFIER: PT. CAHAYA REMBULAN SEJATI ===
+
+[Data Awal]
+Harga Kontrak Murni : 3000000, 5000000, 8000000
+
+[Hasil Modifikasi Callback]
+1. Promo Diskon 20%  : 2400000, 4000000, 6400000
+2. Setelah PPN 11%   : 3330000, 5550000, 8880000
+3. Format Tampilan   : Rp 3.000.000, Rp 5.000.000, Rp 8.000.000
+
+============================================================
+Status HOF Engine   : Execution Success (Zero Duplication)
 */
+const hargaAwal = [3000000, 5000000, 8000000];
 
-const poll = {
-  question: 'What is your favourite programming language?',
-  options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
-
-  // This generates [0, 0, 0, 0]. More in the next section!
-  answers: new Array(4).fill(0),
-
-  displayResult: function (type = 'array') {
-    if (type === 'string') {
-      console.log(`Poll Result Are ${this.answers.join(', ')}`);
-    } else {
-      console.log(this.answers);
-    }
-  },
-  registerNewAnswer: function () {
-    console.log(this.options);
-    console.log(this.question);
-
-    const input = Number(
-      prompt(`${this.question} \n ${this.options.join('\n')}`),
-    );
-
-    if (
-      typeof input === 'number' &&
-      input >= 0 &&
-      input < this.answers.length
-    ) {
-      this.answers[input]++;
-      this.displayResult();
-    } else {
-      console.log(`Invalid Input`);
-    }
-    console.log(this.answers);
-  },
+const diskonKlienSetia = function (harga) {
+  return harga * 0.8;
 };
 
-const getAnswer = poll.registerNewAnswer.bind(poll);
+const tambahPajakPPN = function (harga) {
+  return harga + harga * 0.11;
+};
 
-const pollBtn = document.querySelector('.poll');
-pollBtn.addEventListener('click', getAnswer);
+const formatKeRupiah = function (harga) {
+  return `Rp ${harga.toLocaleString('id-ID')}`;
+};
 
-//BONUS
-const data1 = [5, 2, 3];
-const data2 = [1, 5, 3, 9, 6, 1];
+const modifierHarga = function (hargaDasar, callback) {
+  const hasilModifikasi = [];
 
-poll.displayResult.call({ answers: data1 }, 'string');
-poll.displayResult.call({ answers: data2 }, 'string');
+  for (const harga of hargaDasar) {
+    const hargaBaru = callback(harga);
+    hasilModifikasi.push(hargaBaru);
+  }
+
+  return hasilModifikasi;
+};
+
+const hargaDiskon = modifierHarga(hargaAwal, diskonKlienSetia);
+const hargaPPN = modifierHarga(hargaAwal, tambahPajakPPN);
+const hargaRupiah = modifierHarga(hargaAwal, formatKeRupiah);
+console.log(hargaDiskon);
+
+const execute = function () {
+  console.log(`=== FINANCIAL PRICE MODIFIER: PT. CAHAYA REMBULAN SEJATI ===\n`);
+
+  console.log(`[Data Awal]\nHarga Kontrak Murni : ${hargaAwal.join(', ')}\n`);
+
+  console.log(
+    `[Hasil Modifikasi Callback]\n1. Promo Diskon 20 % : ${hargaDiskon.join(', ')}\n2. Setelah PPN 11% : ${hargaPPN.join(', ')}\n3. Format Tampilan : ${hargaRupiah}`,
+  );
+
+  console.log(`============================================================`);
+};
+
+execute();
