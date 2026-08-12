b7a324132ebef7d1ff0bb1b1761b2c5a9bc2db69
Delano <del.191006@gmail.com>
2026-08-12 20:50:21 +0700

    INITIAL SETUP

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -70,7 +70,6 @@ const currencies = new Map([
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
-
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
