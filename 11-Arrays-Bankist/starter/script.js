7ae9749ed3fb60e84d2c07663f507b0c23f70f66
Delano <del.191006@gmail.com>
2026-08-12 21:38:56 +0700

    1. Array Method : slice,splice,reverse,concat,join

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1,69 +1,6 @@
 'use strict';
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
-
-// Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
-
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
-
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
-
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
-
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
-
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
-
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+// LECTURES #1 Array Method
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -73,3 +10,40 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+//SLICE : Mengekstraksi sebuah bagian dari array (tergantung dari isi argumen) tetapi tidak merubah array aslinya.
+const numArr = [19, 10, 4, 5, 7];
+const get3Num = numArr.slice(0, 3);
+
+console.log(get3Num);
+console.log(numArr); //array numArr tidak berubah
+
+//SPLICE : Mengekstraksi sebuah bagian dari array (tergantung dari isi argumen) dan MERUBAH bentuk array aslinya
+
+// const get2Num = numArr.splice(0, 2);
+
+// console.log(get2Num); // 2 index awal dikeluarkan dari array numArr dan disimpan ke get2Num
+// console.log(numArr); // 2 index diawal hilang, menyisakan [4,5,7]
+
+const getAllNum = numArr.splice(0); //ambil semua isi dari array numArr dan simpan di variable getAllNum
+console.log(getAllNum);
+console.log(numArr); //isinya kosong karena nilainya udah diambil semua sama getAllNum
+
+//REVERSE : membalik urutan nilai pada array. dan MERUBAH bentuk array aslinya
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f'];
+console.log(alphabet.reverse());
+console.log(alphabet);
+
+//concat : Menggabungkan arrays menjadi 1 array. dia sifatnya ga merubah bentuk asli array
+const x = [1, 2, 3, 4, 5];
+const y = [6, 7, 8, 9, 10];
+
+const xy = x.concat(y);
+const xyAlphabet = x.concat(y).concat(alphabet);
+
+console.log(xy);
+console.log(xyAlphabet);
+
+//join : Menggabungkan isi array menjadi 1 nilai dan dipisahkan berdasarkan argumennya
+const allXy = xy.join(',');
+console.log(allXy);
