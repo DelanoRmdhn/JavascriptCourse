48f0b8e5a88024f3e97959eaccd5f4f547074e82
Delano <del.191006@gmail.com>
2026-08-16 00:33:07 +0700

    [BANKIST PROJECT] : Showing Balance to Dashboard & Trick to get Max Value

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1,106 +1,116 @@
 'use strict';
 
-//6. REDUCE : digunakan untuk meringkas semua elemen pada array menjadi 1 nilai.
+/////////////////////////////////////////////////
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+/////////////////////////////////////////////////
+// BANKIST APP
 
-const totalBalance = movements.reduce((sum, balance, i) => {
-  console.log(`Iteration ${i} : ${sum}`);
-  return (sum += balance);
-}, 0);
-
-console.log(totalBalance);
-
-const soal1 = function () {
-  const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-  const balance = movements.reduce((acc, balance) => (acc += balance), 0);
-  console.log(`Saldo Akhir : ${balance}`);
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
 };
 
-soal1();
-
-const soal2 = function () {
-  const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-  const allDeposit = movements.reduce(function (acc, balance) {
-    if (balance > 0) {
-      return (acc += balance);
-    }
-
-    return acc;
-  }, 0);
-
-  console.log(`Semua Deposit : ${allDeposit}`);
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
 };
 
-soal2();
-
-const soal3 = function () {
-  const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-  const totalWithdrawal = movements.reduce(function (acc, balance) {
-    if (balance < 0) {
-      return acc + balance;
-    }
-    return acc;
-  }, 0);
-
-  console.log(`Total Penarikan : ${Math.abs(totalWithdrawal)}`);
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
 };
 
-soal3();
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
 
-const soal4 = function () {
-  const accounts = [
-    {
-      owner: 'Jonas Schmedtmann',
-      movements: [200, 450, -400, 3000],
-    },
-    {
-      owner: 'Jessica Davis',
-      movements: [5000, 3400, -150, -790],
-    },
-    {
-      owner: 'Steven Thomas',
-      movements: [200, -200, 340, -300],
-    },
-  ];
+const accounts = [account1, account2, account3, account4];
 
-  const accountBalance = accounts.map(function ({ movements }) {
-    return movements.reduce((acc, balance) => acc + balance, 0);
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+//Tampilkan History Transaksi
+
+const displayTransactionHistory = function (movements) {
+  //Kosongin Semua elemen yang membungkus class movements
+  containerMovements.innerHTML = '';
+
+  //tampilkan transaksi ke halaman
+  movements.forEach(function (move, i) {
+    //1. pisahkan tipe transaksi
+    const transactionType = move >= 0 ? 'deposit' : 'withdrawal';
+    //2. tag HTML yang mau dimanipulasi & menampilkan value sesuai tipe transaksi
+    const displayMovement = `
+    <div class="movements__row">
+    <div class="movements__type movements__type--${transactionType}">${i + 1} ${transactionType.toUpperCase()}</div>
+    <div class="movements__value">${move}</div>
+    </div>
+    `;
+
+    //3. Masukin hasil manipulasi ke containerMovements agar tampil pada halaman
+    containerMovements.insertAdjacentHTML('afterbegin', displayMovement);
   });
-
-  console.log(accountBalance);
 };
 
-soal4();
-
-const soal5 = function () {
-  const accounts = [
-    {
-      owner: 'Jonas Schmedtmann',
-      movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-    },
-    {
-      owner: 'Jessica Davis',
-      movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-    },
-    {
-      owner: 'Steven Thomas',
-      movements: [200, -200, 340, -300, -20, 50, 400, -460],
-    },
-  ];
-
-  const allBalance = accounts
-    .map(function ({ movements }) {
-      return movements.reduce((acc, balance) => acc + balance, 0);
-    })
-    .reduce(function (acc, balance) {
-      return acc + balance;
-    }, 0);
-
-  console.log(`Saldo Semua Akun : ${allBalance}`);
+const generateUsernames = function (accounts) {
+  accounts.forEach(function (account) {
+    account.username = account.owner
+      .toLowerCase()
+      .split(' ')
+      .map(char => char[0])
+      .join('');
+  });
 };
 
-soal5();
+const showBalance = function (account) {
+  const balance = account
+    .filter(balance => balance >= 0)
+    .reduce((acc, balance) => acc + balance, 0);
+
+  labelBalance.textContent = `${balance}€`;
+  console.log(balance);
+};
+
+displayTransactionHistory(accounts[0].movements);
+generateUsernames(accounts);
+showBalance(account1.movements);
+
+const getMaxValue = account1.movements.reduce(function (acc, balance) {
+  if (acc > balance) return acc;
+  else return balance;
+}, account1.movements[0]);
+
+console.log(getMaxValue);
