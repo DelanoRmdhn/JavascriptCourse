8624d0da7dfd3207b0cb603a8c84dc63ed0215c0
Delano <del.191006@gmail.com>
2026-08-17 22:38:00 +0700

    2. FindIndex (Array Method) : Pengertian findIndex, Cara Penggunaan, dan Latihan Soal

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1,16 +1,9 @@
-'use strict';
-
-/////////////////////////////////////////////////
-
-/////////////////////////////////////////////////
-// BANKIST APP
-
-// Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  type: 'premium',
 };
 
 const account2 = {
@@ -18,6 +11,7 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  type: 'standard',
 };
 
 const account3 = {
@@ -25,6 +19,7 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  type: 'premium',
 };
 
 const account4 = {
@@ -32,188 +27,75 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  type: 'basic',
 };
 
-const accounts = [account1, account2, account3, account4];
+// const accounts = [account1, account2, account3, account4];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+//2.findIndex() : digunakan untuk mencari index / posisi dari suatu elemen pada sebuah array yang memenuhi kondisi yang diberikan.
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
+const soal1 = function () {
+  const accounts = [
+    {
+      owner: 'Jonas Schmedtmann',
+      username: 'js',
+      movements: [200, 450, -400],
+    },
+    {
+      owner: 'Jessica Davis',
+      username: 'jd',
+      movements: [5000, -150],
+    },
+    {
+      owner: 'Steven Thomas Williams',
+      username: 'stw',
+      movements: [200, -200, 340],
+    },
+    {
+      owner: 'Sarah Smith',
+      username: 'ss',
+      movements: [430, 1000],
+    },
+  ];
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+  const removeAccount = function (usn) {
+    const getAccountIndex = accounts.findIndex(
+      ({ username }) => username === usn,
+    );
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+    if (getAccountIndex === -1) return;
 
-//GLOBAL VARIABLE
-let currentAccount;
-//Buat Username untuk tiap akun
-const generateUsernames = function (accounts) {
-  accounts.forEach(function (account) {
-    account.username = account.owner
-      .toLowerCase()
-      .split(' ')
-      .map(char => char[0])
-      .join('');
-  });
+    accounts.splice(getAccountIndex, 1);
+    accounts.forEach(acc => console.log(acc.owner.split(' ')[0]));
+  };
+
+  removeAccount('stw');
 };
 
-generateUsernames(accounts);
-//Fitur Login
-const login = function (e) {
-  e.preventDefault();
-  //get value
-  const username = inputLoginUsername.value;
-  const pin = inputLoginPin.value;
+soal1();
 
-  //verifikasi username dan pin
-  currentAccount = verifyUser(username, pin);
+const soal2 = function () {
+  const transactions = [
+    { id: 101, type: 'deposit', amount: 500 },
+    { id: 102, type: 'withdrawal', amount: 200 },
+    { id: 103, type: 'deposit', amount: 1000 },
+    { id: 104, type: 'withdrawal', amount: 300 },
+  ];
 
-  //Tampilkan Informasi Informasi Akun
-  if (currentAccount) {
-    showAccountInformation(currentAccount);
-  } else return;
+  const cancelTransaction = function (transactionId) {
+    const getTransactionIndex = transactions.findIndex(
+      ({ id }) => id === transactionId,
+    );
+
+    if (getTransactionIndex === -1) return;
+
+    transactions.splice(getTransactionIndex, 1);
+    transactions.forEach(({ id, type, amount }) =>
+      console.log(`${id} ${type} ${amount}`),
+    );
+  };
+  cancelTransaction(103);
+  cancelTransaction(999);
 };
-btnLogin.addEventListener('click', login);
-
-//Fitur Transfer
-
-const transfer = function (e) {
-  e.preventDefault();
-
-  const inputDestinationAccount = inputTransferTo.value;
-  const transferAmount = Number(inputTransferAmount.value);
-
-  const destinationAccount = verifyUsername(inputDestinationAccount);
-  console.log(destinationAccount);
-
-  if (!destinationAccount) return;
-
-  //validate Transfer
-  const isValid = validateTransfer(
-    currentAccount,
-    destinationAccount,
-    transferAmount,
-  );
-
-  if (!isValid) return;
-  //lakukan transfer dan update UI
-  transferBalance(currentAccount, destinationAccount, transferAmount);
-  showAccountInformation(currentAccount);
-};
-
-btnTransfer.addEventListener('click', transfer);
-
-const validateTransfer = function (
-  account,
-  destinationAccount,
-  transferAmount,
-) {
-  //Dapatkan Jumlah Saldo currentAccoun
-  const currentBalance = calcCurrentBalance(account.movements);
-  console.log(currentBalance);
-
-  //Validasi berupa : transferAmount harus > 0, currentBalance > transferAmount & gaboleh transfer ke diri sendiri
-  return (
-    transferAmount > 0 &&
-    currentBalance >= transferAmount &&
-    destinationAccount.username !== account.username
-  );
-};
-
-const transferBalance = function (
-  currentAccount,
-  destinationAccount,
-  transferAmount,
-) {
-  currentAccount.movements.push(-transferAmount);
-
-  destinationAccount.movements.push(transferAmount);
-};
-
-const verifyUsername = function (inputUsername) {
-  return accounts.find(({ username }) => inputUsername === username);
-};
-const verifyUser = function (inputUsername, inputPin) {
-  return accounts.find(
-    acc => acc.username === inputUsername && acc.pin === Number(inputPin),
-  );
-};
-
-const showAccountInformation = function (account) {
-  //styling
-  containerApp.style.opacity = 1;
-  inputLoginUsername.value = '';
-  inputLoginPin.value = '';
-  inputTransferTo.value = '';
-  inputTransferAmount.value = '';
-  labelWelcome.textContent = `Good Afternoon, ${account.owner.split(' ')[0]}!`;
-
-  //displaying account Information
-  displayTransactionHistory(account.movements);
-  const balance = calcCurrentBalance(account.movements);
-  labelBalance.textContent = `${balance}€`;
-
-  accountSummary(account);
-};
-
-//Tampilkan History Transaksi
-const displayTransactionHistory = function (movements) {
-  //Kosongin Semua elemen yang membungkus class movements
-  containerMovements.innerHTML = '';
-
-  // tampilkan transaksi ke halaman
-  movements.forEach(function (move, i) {
-    //1. pisahkan tipe transaksi
-    const transactionType = move >= 0 ? 'deposit' : 'withdrawal';
-    //2. tag HTML yang mau dimanipulasi & menampilkan value sesuai tipe transaksi
-    const displayMovement = `
-    <div class="movements__row">
-    <div class="movements__type movements__type--${transactionType}">${i + 1} ${transactionType.toUpperCase()}</div>
-    <div class="movements__value">${move}</div>
-    </div>
-    `;
-
-    //3. Masukin hasil manipulasi ke containerMovements agar tampil pada halaman
-    containerMovements.insertAdjacentHTML('afterbegin', displayMovement);
-  });
-};
-
-//Hitung & Tampilkan Saldo
-const calcCurrentBalance = function (movements) {
-  return movements.reduce((acc, move) => acc + move, 0);
-};
-
-// Hitung & Tampilkan accountSummary
-const accountSummary = function (account) {
-  //incomes
-  const income = account.movements
-    .filter(balance => balance > 0)
-    .reduce((acc, balance) => acc + balance, 0);
-  labelSumIn.textContent = `${income}€`;
-
-  const outcome = account.movements
-    .filter(balance => balance < 0)
-    .reduce((acc, balance) => acc + balance);
-  labelSumOut.textContent = `${Math.abs(outcome)}€`;
-
-  const interest = income * (account.interestRate / 100);
-  labelSumInterest.textContent = `${interest}€`;
-};
+soal2();
