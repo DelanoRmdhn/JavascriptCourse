5f103799ec6e9b6bfa15d5f8a033cb2d3641f72b
Delano <del.191006@gmail.com>
2026-08-19 23:52:30 +0700

    3. Cara Lain dalam Membuat & Mengisi Array (new Array(), fill(), Array.from())

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1,212 +1,79 @@
 'use strict';
 
-//LATIHAN SOAL GROUP BY
+/////////////////////////////////////////////////
 
-/*
-🟢 Latihan 1 — Transaction Grouping ⭐⭐
+/////////////////////////////////////////////////
+// BANKIST APP
 
-Diberikan:
-
-const movements = [
-  200,
-  -450,
-  700,
-  -1200,
-  3000,
-  -150,
-  850,
-];
-
-Gunakan Object.groupBy() untuk mengelompokkan transaksi menjadi:
-
-deposit → transaksi positif
-withdrawal → transaksi negatif
-
-Expected:
-
-{
-  deposit: [200, 700, 3000, 850],
-  withdrawal: [-450, -1200, -150]
-}
-
-Constraint:
-
-Gunakan Object.groupBy()
-Jangan gunakan filter()
-Jangan mengubah movements asli
-*/
-
-const soal1 = function () {
-  const movements = [200, -450, 700, -1200, 3000, -150, 850];
-
-  const groupByMovementsType = Object.groupBy(movements, mov =>
-    mov > 0 ? 'deposit' : 'withdrawal',
-  );
-
-  console.log(`Deposit : ${groupByMovementsType.deposit}`);
-  console.log(`Withdrawal : ${groupByMovementsType.withdrawal}`);
-};
-soal1();
-
-/*
-🟠 Latihan 2 — Account Classification ⭐⭐⭐
-
-Diberikan:
-
-const accounts = [
-  {
-    owner: 'Jonas',
-    movements: [200, 450, -400, 3000],
-  },
-  {
-    owner: 'Jessica',
-    movements: [5000, -300, 2000],
-  },
-  {
-    owner: 'Steven',
-    movements: [-200, -500, 100],
-  },
-  {
-    owner: 'Sarah',
-    movements: [1000, 500, -200],
-  },
-];
-
-Kelompokkan account berdasarkan apakah balance mereka positif atau negatif.
-
-Balance dihitung menggunakan reduce().
-
-Expected bentuk hasil:
-
-{
-  positive: [
-    // account yang balance-nya > 0
-  ],
-  negative: [
-    // account yang balance-nya < 0
-  ]
-}
-
-Constraint:
-
-Kamu perlu menggabungkan:
-
-reduce
-   ↓
-tentukan kategori
-   ↓
-groupBy
-
-Jangan menambahkan property balance ke object original.
-*/
-
-const soal2 = function () {
-  const accounts = [
-    {
-      owner: 'Jonas',
-      movements: [200, 450, -400, 3000],
-    },
-    {
-      owner: 'Jessica',
-      movements: [5000, -300, 2000],
-    },
-    {
-      owner: 'Steven',
-      movements: [-200, -500, 100],
-    },
-    {
-      owner: 'Sarah',
-      movements: [1000, 500, -200],
-    },
-  ];
-
-  const groupByBalance = Object.groupBy(accounts, acc => {
-    const balance = acc.movements.reduce((add, mov) => add + mov, 0);
-
-    if (balance > 0) return 'positive';
-    else return 'negative';
-  });
-
-  console.log(groupByBalance);
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
 };
 
-soal2();
-
-/*
-🔴 Latihan 3 — Bankist Transaction Groups ⭐⭐⭐⭐
-
-Diberikan:
-
-const movements = [
-  200,
-  -450,
-  700,
-  -1200,
-  3000,
-  -150,
-  850,
-  -75,
-  4200,
-];
-
-Kelompokkan transaksi menjadi 3 kategori:
-
-small
-medium
-large
-
-Aturannya:
-
-|movement| < 500
-→ small
-
-
-500 <= |movement| < 2000
-→ medium
-
-
-|movement| >= 2000
-→ large
-
-Expected:
-
-{
-  small: [
-    200,
-    -450,
-    -150,
-    -75
-  ],
-
-
-  medium: [
-    700,
-    -1200,
-    850
-  ],
-
-
-  large: [
-    3000,
-    4200
-  ]
-}
-
-Hint: Karena transaksi bisa negatif, perhatikan penggunaan:
-
-Math.abs()
-*/
-
-const soal3 = function () {
-  const movements = [200, -450, 700, -1200, 3000, -150, 850, -75, 4200];
-
-  const groupByBalanceTransaction = Object.groupBy(movements, move => {
-    if (Math.abs(move) < 500) return 'small';
-    if (Math.abs(move) >= 500 && Math.abs(move) <= 2000) return 'medium';
-    if (Math.abs(move) >= 2000) return 'large';
-  });
-
-  console.log(groupByBalanceTransaction);
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
 };
 
-soal3();
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, -509],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+////////////////////////////
+//CARA LAIN DALAM MEMBUAT ARRAY
+////////////////////////////
+
+//1.new Array() : Digunakan untuk membuat sebuah array
+
+//membuat sebuah array kosong dengan panjang 5
+const x = new Array(5);
+console.log(x);
+
+//membuat sebuah array standar
+const y = new Array(1, 2, 3, 4, 5);
+console.log(y);
+
+//2. fill() : fill digunakan untuk mengisi array dengan suatu nilai
+
+//mengisi array kosong dengan isi berupa sebuah nilai yang sama sepanjang array
+x.fill(1);
+console.log(x);
+
+//bentuk lain fill()
+const movements = [100, 200, 300, 450];
+console.log(movements);
+
+movements.fill(400, -1);
+console.log(movements);
+
+movements.fill(200, 1, 3);
+console.log(movements);
+
+//3. Array.from() : adalah static method milik array yang digunakan untuk membuat array
+
+//membuat array berdasarkan sebuah nilai
+const nama = 'Delano';
+const alfabet = Array.from(nama);
+console.log(alfabet);
+
+//
+const nums = Array.from({ length: 5 }, (_, i) => i + 1);
+console.log(nums);
+
+console.log(Array.from('12345', value => Number(value) * 2));
