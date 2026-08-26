1cb4402cdd3deb52025f7c045559ce65f016e352
Delano <del.191006@gmail.com>
2026-08-26 09:27:33 +0700

    [BANKIST APP] : Implementing Dates to transaction History & Current date

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -51,381 +51,277 @@ const account2 = {
 
 const accounts = [account1, account2];
 
-////////////////////////////////////
-//DATE & TIME
-////////////////////////////////////
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
 
-/*
-🧪 Challenge 1 — User Registration Date
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
 
-Kamu sedang membuat sistem registrasi user.
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
 
-Data user:
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
 
-const user = {
-  name: 'Delano',
-  registrationDate: new Date(),
+//GLOBAL VARIABLE
+let currentAccount;
+let sorted = false;
+
+//Buat Username untuk tiap akun
+const generateUsernames = function (accounts) {
+  accounts.forEach(function (account) {
+    account.username = account.owner
+      .toLowerCase()
+      .split(' ')
+      .map(char => char[0])
+      .join('');
+  });
 };
-🎯 Tugas
 
-Buat function soal1 yang menampilkan:
+generateUsernames(accounts);
 
-User          : Delano
-Year          : 2026
-Month         : 8
-Date          : 26
-Day of Week   : 3
-Hour          : 7
-Minute        : 24
+//Fitur Login
+const login = function (e) {
+  e.preventDefault();
+  //get value
+  const username = inputLoginUsername.value;
+  const pin = inputLoginPin.value;
 
-Angka di atas adalah contoh berdasarkan waktu saat soal dibuat. Jangan hardcode angka tersebut. Program harus mengambilnya dari registrationDate.
+  //verifikasi username dan pin
+  currentAccount = verifyUser(username, pin);
 
-Requirements
-Gunakan new Date() untuk mendapatkan waktu registrasi.
-Ambil:
-tahun
-bulan
-tanggal
-hari dalam minggu
-jam
-menit
-Gunakan method get...() yang sesuai.
-Ingat bahwa bulan JavaScript dimulai dari 0.
+  //Tampilkan Informasi Informasi Akun
+  if (currentAccount) {
+    showAccountInformation(currentAccount);
+  } else return;
+};
+btnLogin.addEventListener('click', login);
 
-Ingat perbedaan:
+//Fitur Transfer
+const transfer = function (e) {
+  e.preventDefault();
 
-getDate()
-getDay()
-*/
+  const inputDestinationAccount = inputTransferTo.value;
+  const transferAmount = Number(inputTransferAmount.value);
 
-const soal1 = function () {
-  const user = {
-    name: 'Delano',
-    registrationDate: new Date(),
-  };
+  const destinationAccount = verifyUsername(inputDestinationAccount);
+  console.log(destinationAccount);
 
-  console.log(user.registrationDate);
-  const year = user.registrationDate.getFullYear();
-  const month = user.registrationDate.getMonth() + 1;
-  const date = user.registrationDate.getDate();
-  const day = user.registrationDate.getDay();
-  const hour = user.registrationDate.getHours();
-  const minute = user.registrationDate.getMinutes();
+  if (!destinationAccount) return;
 
-  console.log(
-    `User \t\t\t : ${user.name}\n` +
-      `Year \t\t\t : ${year}\n` +
-      `Month \t\t\t : ${month}\n` +
-      `Date \t\t\t : ${date}\n` +
-      `Day Of Week \t : ${day}\n` +
-      `Hour \t\t\t : ${hour}\n` +
-      `Minute \t\t\t : ${minute}`,
+  //validate Transfer
+  const isValid = validateTransfer(
+    currentAccount,
+    destinationAccount,
+    transferAmount,
+  );
+
+  if (!isValid) return;
+  //lakukan transfer dan update UI
+  transferBalance(currentAccount, destinationAccount, transferAmount);
+  showAccountInformation(currentAccount);
+};
+
+btnTransfer.addEventListener('click', transfer);
+
+//Fitur Loan
+const loan = function (e) {
+  e.preventDefault();
+
+  const loanAmount = Number(inputLoanAmount.value);
+
+  const loanApproved = currentAccount.movements.some(
+    movement => movement >= loanAmount * 0.1,
+  );
+
+  if (loanAmount > 0 && loanApproved) {
+    const now = new Date().toISOString();
+
+    currentAccount.movements.push(loanAmount);
+    currentAccount.movementsDates.push(now);
+    showAccountInformation(currentAccount);
+  }
+};
+
+btnLoan.addEventListener('click', loan);
+
+//Fitur Tutup Account
+const closeAccount = function (e) {
+  e.preventDefault();
+
+  const inputUsername = inputCloseUsername.value;
+  const inputPin = inputClosePin.value;
+
+  if (
+    verifyUser(inputUsername, inputPin) &&
+    currentAccount.username === inputUsername
+  ) {
+    const getIndex = accounts.findIndex(acc => acc.username === inputUsername);
+
+    accounts.splice(getIndex, 1);
+    currentAccount = null;
+
+    containerApp.style.opacity = 0;
+    inputClosePin.value = '';
+    inputCloseUsername.value = '';
+    labelWelcome.textContent = `Log in to get started`;
+  }
+};
+
+btnClose.addEventListener('click', closeAccount);
+
+//FITUR SORT
+const sort = function () {
+  if (!sorted) {
+    const sortedMovements = [...currentAccount.movements].sort((a, b) => a - b);
+
+    displayTransactionHistory({
+      ...currentAccount,
+      movements: sortedMovements,
+    });
+
+    sorted = true;
+  } else {
+    displayTransactionHistory(currentAccount);
+    sorted = false;
+  }
+};
+btnSort.addEventListener('click', sort);
+
+//HELPER FUNCTION
+const validateTransfer = function (
+  account,
+  destinationAccount,
+  transferAmount,
+) {
+  //Dapatkan Jumlah Saldo currentAccoun
+  const currentBalance = calcCurrentBalance(account.movements);
+  console.log(currentBalance);
+
+  //Validasi berupa : transferAmount harus > 0, currentBalance > transferAmount & gaboleh transfer ke diri sendiri
+  return (
+    transferAmount > 0 &&
+    currentBalance >= transferAmount &&
+    destinationAccount.username !== account.username
   );
 };
 
-soal1();
+const transferBalance = function (
+  currentAccount,
+  destinationAccount,
+  transferAmount,
+) {
+  const now = new Date().toISOString();
+  currentAccount.movements.push(-transferAmount);
+  currentAccount.movementsDates.push(now);
 
-/* 
-🧪 Challenge 2 — Birthday Information
-
-Buat sebuah object:
-
-const user = {
-  name: 'Delano',
-  birthDate: new Date(2006, 4, 17),
+  destinationAccount.movements.push(transferAmount);
+  destinationAccount.movementsDates.push(now);
 };
-Tugas
 
-Tampilkan:
-
-Name          : Delano
-Birth Year    : 2006
-Birth Month   : May
-Birth Date    : 17
-
-Syarat tambahan: jangan hardcode "May".
-
-Gunakan array:
-
-const months = [
-  'January',
-  'February',
-  'March',
-  // ...
-];
-
-Kemudian gunakan getMonth() untuk mengambil nama bulan.
-
-💡 Hint
-
-Ingat:
-
-new Date(2006, 4, 17)
-
-bukan April. 😉
-*/
-
-const soal2 = function () {
-  const user = {
-    name: 'Delano',
-    birthDate: new Date(2006, 4, 17),
-  };
-
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-
-  console.log(
-    `Name \t\t\t : ${user.name}\n` +
-      `Birth Year \t\t : ${user.birthDate.getFullYear()}\n` +
-      `Birth Month \t : ${months[user.birthDate.getMonth()]}\n` +
-      `Birth Date \t\t : ${user.birthDate.getDate()} `,
+const verifyUsername = function (inputUsername) {
+  return accounts.find(({ username }) => inputUsername === username);
+};
+const verifyUser = function (inputUsername, inputPin) {
+  return accounts.find(
+    acc => acc.username === inputUsername && acc.pin === Number(inputPin),
   );
 };
 
-soal2();
+const showAccountInformation = function (account) {
+  //styling
+  containerApp.style.opacity = 1;
+  inputLoginUsername.value = '';
+  inputLoginPin.value = '';
+  inputTransferTo.value = '';
+  inputTransferAmount.value = '';
+  labelWelcome.textContent = `Good Afternoon, ${account.owner.split(' ')[0]}!`;
 
-/*
-🧪 Challenge 3 — Meeting Schedule
+  //displaying account Information
+  displayTransactionHistory(account);
+  const balance = calcCurrentBalance(account.movements);
+  labelBalance.textContent = `${balance}€`;
 
-Kamu ingin membuat jadwal meeting:
-
-const meeting = new Date(2026, 7, 26, 14, 30);
-
-Buat output:
-
-Meeting Date : 26/08/2026
-Meeting Time : 14:30
-Requirements
-
-Gunakan:
-
-getFullYear()
-getMonth()
-getDate()
-getHours()
-getMinutes()
-
-Untuk sementara jangan menggunakan Intl.DateTimeFormat().
-
-⭐ Bonus
-
-Buat agar tanggal dan waktu selalu memiliki 2 digit.
-
-Misalnya:
-
-08
-
-bukan:
-
-8
-
-Hint:
-
-String(...).padStart(2, '0')
-Urutan pengerjaan yang aku sarankan
-
-Jangan langsung kerjakan semuanya.
-
-Mulai:
-
-Challenge 1
-   ↓
-cek pemahaman get...
-   ↓
-Challenge 2
-   ↓
-pahami month index
-   ↓
-Challenge 3
-   ↓
-gabungkan Date + formatting
-*/
-
-const soal3 = function () {
-  const meeting = new Date(2026, 7, 26, 14, 30);
-
-  const date = `${meeting.getDate()}/${String(meeting.getMonth() + 1).padStart(2, '0')}/${meeting.getFullYear()}`;
-  const time = `${String(meeting.getHours()).padStart(2, '0')}:${String(meeting.getMinutes()).padStart(2, '0')}`;
-
-  console.log(`Meeting Date \t : ${date}\n` + `Meeting Time \t : ${time}`);
+  accountSummary(account);
 };
 
-soal3();
+//Tampilkan History Transaksi
+const displayTransactionHistory = function (account) {
+  //Kosongin Semua elemen yang membungkus class movements
+  containerMovements.innerHTML = '';
 
-/*
-🧪 Challenge 4 — Days Until Deadline
+  // tampilkan transaksi ke halaman
+  account.movements.forEach(function (move, i) {
+    //1. pisahkan tipe transaksi
+    const transactionType = move >= 0 ? 'deposit' : 'withdrawal';
 
-Kamu sedang membuat sistem deadline tugas kuliah.
+    const date = new Date(account.movementsDates[i]);
 
-const soal4 = function () {
-  const today = new Date(2026, 7, 26);
-  const deadline = new Date(2026, 8, 5);
+    //2. tag HTML yang mau dimanipulasi & menampilkan value sesuai tipe transaksi
+    const displayMovement = `
+    <div class="movements__row">
+    <div class="movements__type movements__type--${transactionType}">${i + 1} ${transactionType.toUpperCase()}</div>
+    <div class="movements__date">${formatDate(date)}</div>
+    <div class="movements__value">${move}</div>
+    </div>
+    `;
 
-  // TODO
+    //3. Masukin hasil manipulasi ke containerMovements agar tampil pada halaman
+    containerMovements.insertAdjacentHTML('afterbegin', displayMovement);
+  });
 };
 
-soal4();
-🎯 Tugas
-
-Hitung berapa hari lagi sampai deadline.
-
-Output yang diharapkan:
-
-Today       : 26/08/2026
-Deadline    : 05/09/2026
-Days Left   : 10 days
-
-Requirements
-Jangan menghitung tanggal secara manual.
-Gunakan getTime() atau unary + untuk mendapatkan timestamp.
-
-Hitung selisih:
-deadline - today
-Ingat bahwa hasil pengurangan dua Date adalah milliseconds.
-Konversikan milliseconds → days.
-Gunakan Math.round() untuk mendapatkan jumlah hari bulat.
-Format tanggal menjadi DD/MM/YYYY seperti Challenge 3.
-💡 Hint 1 — Timestamp
-
-Kamu bisa mendapatkan timestamp dengan:
-
-today.getTime()
-
-dan:
-
-deadline.getTime()
-
-Kemudian:
-
-const difference = deadline.getTime() - today.getTime();
-*/
-
-const soal4 = function () {
-  const today = new Date(2026, 7, 26);
-  const deadline = new Date(2026, 8, 5);
-
-  const difference = deadline.getTime() - today.getTime();
-
-  const dayLeft = Math.round(difference / (1000 * 60 * 60 * 24));
-  console.log(
-    `Today \t\t\t : ${String(today.getDate()).padStart(2, '0')}/${String(today.getMonth() + 1).padStart(2, '0')}/${today.getFullYear()}\n` +
-      `Deadline \t\t : ${String(deadline.getDate()).padStart(2, '0')}/${String(deadline.getMonth() + 1).padStart(2, '0')}/${deadline.getFullYear()}\n` +
-      `Days Left \t\t : ${dayLeft}`,
-  );
+//Hitung & Tampilkan Saldo
+const calcCurrentBalance = function (movements) {
+  return movements.reduce((acc, move) => acc + move, 0);
 };
 
-soal4();
+// Hitung & Tampilkan accountSummary
+const accountSummary = function (account) {
+  // IN
+  const income = account.movements
+    .filter(movement => movement > 0)
+    .reduce((acc, movement) => acc + movement, 0);
 
-/*
-🧪 Challenge 5 — Subscription Expiration
+  labelSumIn.textContent = `${income.toFixed(2)}€`;
 
-Kamu sedang membuat sistem subscription.
+  // OUT
+  const outcome = account.movements
+    .filter(movement => movement < 0)
+    .reduce((acc, movement) => acc + movement, 0);
 
-Seorang user mulai berlangganan pada:
+  labelSumOut.textContent = `${Math.abs(outcome).toFixed(2)}€`;
 
-const subscriptionStart = new Date(2026, 7, 26);
+  // INTEREST
+  const interest = account.movements
+    .filter(movement => movement > 0)
+    .map(deposit => (deposit * account.interestRate) / 100)
+    .filter(interest => interest >= 1)
+    .reduce((acc, interest) => acc + interest, 0);
 
-Subscription berlaku selama 30 hari.
-
-🎯 Tugas
-
-Buat function soal5 yang menghasilkan:
-
-Subscription Start : 26/08/2026
-Expiration Date    : 25/09/2026
-Requirements
-Buat subscriptionStart dengan:
-new Date(2026, 7, 26)
-Buat Date baru untuk expiration.
-
-⚠️ Jangan mengubah subscriptionStart secara langsung.
-
-Gunakan:
-setDate()
-
-untuk menambahkan 30 hari.
-
-Format tanggal menjadi:
-DD/MM/YYYY
-Gunakan padStart() seperti Challenge 3 & 4.
-💡 Hint
-
-Kamu bisa membuat copy dari Date:
-
-const expirationDate = new Date(subscriptionStart);
-
-Kemudian:
-
-expirationDate.setDate(
-  expirationDate.getDate() + 30
-);
-
-Kenapa kita melakukan:
-
-expirationDate.getDate() + 30
-
-bukan:
-
-expirationDate.setDate(30);
-
-Karena kita ingin menambahkan 30 hari, bukan menetapkan tanggal menjadi tanggal 30.
-
-⚠️ Konsep penting
-
-Perhatikan ini:
-
-const date = new Date(2026, 7, 26);
-
-date.setDate(date.getDate() + 30);
-
-JavaScript akan otomatis menangani ketika tanggal melewati akhir bulan.
-
-Kamu tidak perlu menghitung:
-
-26 Agustus
-+ 5 hari → 31 Agustus
-+ 25 hari → 25 September
-
-JavaScript yang mengurusnya.
-
-⭐ Bonus
-
-Setelah berhasil, coba ubah:
-
-const subscriptionStart = new Date(2026, 7, 26);
-
-menjadi:
-
-const subscriptionStart = new Date(2026, 11, 15);
-
-dan lihat apakah JavaScript bisa menangani perpindahan tahun ketika menambahkan 30 hari.
-*/
-
-const soal5 = function () {
-  const subscriptionStart = new Date(2026, 11, 15);
-  const expirationDate = new Date(subscriptionStart);
-
-  expirationDate.setDate(expirationDate.getDate() + 30);
-
-  console.log(
-    `${String(subscriptionStart.getDate()).padStart(2, '0')}/${String(subscriptionStart.getMonth() + 1).padStart(2, '0')}/${subscriptionStart.getFullYear()}`,
-  );
-
-  console.log(
-    `${String(expirationDate.getDate()).padStart(2, '0')}/${String(expirationDate.getMonth() + 1).padStart(2, '0')}/${expirationDate.getFullYear()}`,
-  );
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
-soal5();
+const formatDate = function (date) {
+  return `${String(date.getDate()).padStart(2, '0')}/${String(date.getMonth() + 1).padStart(2, '0')}/${date.getFullYear()}`;
+};
+
+/////////////////////////
+const now = new Date();
+const formatNow = `${formatDate(now)}, ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}`;
+labelDate.textContent = formatNow;
+/////////////////////////
